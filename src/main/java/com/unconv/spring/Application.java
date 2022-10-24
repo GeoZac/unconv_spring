56d90fab3328627179e927be032173684e5de926
George Zacharia <george.zcharia@gmail.com>
2022-10-24 17:44:09 +0530

    Initial commit

---
+++ src/main/java/com/unconv/spring/Application.java
@@ -0,0 +1,15 @@
+package com.unconv.spring;
+
+import com.unconv.spring.config.ApplicationProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties({ApplicationProperties.class})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/test/java/com/unconv/spring/ApplicationIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.unconv.spring;
+
+import com.unconv.spring.common.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+class ApplicationIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {}
+}
