b279faf88a7789e6a0c87a9733e42931c1c8a947
George Zacharia <george.zcharia@gmail.com>
2022-11-25 23:44:34 +0530

    domain: Passenger: Adapt to a more real-world like condition
    Helps to bring out more salient features of java

--- src/main/java/com/unconv/spring/domain/Passenger.java
+++ src/main/java/com/unconv/spring/domain/Passenger.java
@@ -1,12 +1,17 @@
 package com.unconv.spring.domain;
 
+import com.unconv.spring.consts.Gender;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,6 +30,22 @@ public class Passenger {
     private Long id;
 
     @Column(nullable = false)
-    @NotEmpty(message = "Text cannot be empty")
-    private String text;
+    @NotEmpty(message = "First name cannot be empty")
+    private String firstName;
+
+    @Column private String middleName;
+
+    @Column(nullable = false)
+    @NotEmpty(message = "Last name cannot be empty")
+    private String lastName;
+
+    @Column(nullable = false)
+    @Digits(fraction = 0, integer = 10, message = "Age must be a number and not greater than 200")
+    @NotNull(message = "Age cannot be empty")
+    private int age;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(nullable = false)
+    @NotNull(message = "Gender cannot be null")
+    private Gender gender;
 }
