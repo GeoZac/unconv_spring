7caf21b3c046a640e37584170dc7b06f9c5fd719
George Zacharia <george.zcharia@gmail.com>
2022-12-13 18:20:42 +0530

    persistence: PassengerRepository: Fetch return of a DELETE transaction

--- src/main/java/com/unconv/spring/persistence/PassengerRepository.java
+++ src/main/java/com/unconv/spring/persistence/PassengerRepository.java
@@ -3,4 +3,6 @@ package com.unconv.spring.persistence;
 import com.unconv.spring.domain.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PassengerRepository extends JpaRepository<Passenger, Long> {}
+public interface PassengerRepository extends JpaRepository<Passenger, Long> {
+    boolean removeById(Long id);
+}
