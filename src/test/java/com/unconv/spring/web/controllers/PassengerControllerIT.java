d01f3124236afad05402663a8bf7393694e02e70
George Zacharia <george.zcharia@gmail.com>
2022-12-13 19:04:39 +0530

    PassengerControllerIT: Adapt tests for the new structure

--- src/test/java/com/unconv/spring/web/controllers/PassengerControllerIT.java
+++ src/test/java/com/unconv/spring/web/controllers/PassengerControllerIT.java
@@ -135,7 +135,8 @@ class PassengerControllerIT extends AbstractIntegrationTest {
         this.mockMvc
                 .perform(delete("/Passenger/{id}", passenger.getId()))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.firstName", is(passenger.getFirstName())));
+                .andExpect(jsonPath("$.data.firstName", is(passenger.getFirstName())))
+                .andExpect(jsonPath("$.wasDeleted", is(true)));
     }
 
     @Test
