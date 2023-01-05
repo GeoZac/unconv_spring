2f9ddde2dc3a2c64282b549d32dbe004401982a7
George Zacharia <george.zcharia@gmail.com>
2023-01-05 09:05:45 +0530

    Offer: Create a OfferDTO class and wrap around the entity

---
+++ src/main/java/com/unconv/spring/dto/OfferDTO.java
@@ -0,0 +1,24 @@
+package com.unconv.spring.domain;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OfferDTO {
+
+    private Long id;
+
+    @Pattern(regexp = "^0x(?:[0-9a-fA-F]{3,4}){1,2}$")
+    @NotEmpty(message = "Badge color cannot be empty")
+    private String badgeColor;
+
+    @NotEmpty(message = "Description cannot be empty")
+    private String description;
+}
