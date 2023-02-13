f648e0230f755b080c32fd5fd9de29a0a6bed036
George Zacharia <george.zcharia@gmail.com>
2023-02-13 06:49:00 +0530

    tests: Add basic tests for OrderProducts

---
+++ src/test/java/com/unconv/spring/web/controllers/OrderProductControllerIT.java
@@ -0,0 +1,129 @@
+package com.unconv.spring.web.controllers;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.unconv.spring.common.AbstractIntegrationTest;
+import com.unconv.spring.domain.OrderProduct;
+import com.unconv.spring.persistence.OrderProductRepository;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+class OrderProductControllerIT extends AbstractIntegrationTest {
+
+    @Autowired private OrderProductRepository orderProductRepository;
+
+    private List<OrderProduct> orderProductList = null;
+
+    @BeforeEach
+    void setUp() {
+        orderProductRepository.deleteAllInBatch();
+
+        orderProductList = new ArrayList<>();
+        orderProductList.add(new OrderProduct(null, "First OrderProduct"));
+        orderProductList.add(new OrderProduct(null, "Second OrderProduct"));
+        orderProductList.add(new OrderProduct(null, "Third OrderProduct"));
+        orderProductList = orderProductRepository.saveAll(orderProductList);
+    }
+
+    @Test
+    void shouldFetchAllOrderProducts() throws Exception {
+        this.mockMvc
+                .perform(get("/OrderProduct"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.size()", is(orderProductList.size())))
+                .andExpect(jsonPath("$.totalElements", is(3)))
+                .andExpect(jsonPath("$.pageNumber", is(1)))
+                .andExpect(jsonPath("$.totalPages", is(1)))
+                .andExpect(jsonPath("$.isFirst", is(true)))
+                .andExpect(jsonPath("$.isLast", is(true)))
+                .andExpect(jsonPath("$.hasNext", is(false)))
+                .andExpect(jsonPath("$.hasPrevious", is(false)));
+    }
+
+    @Test
+    void shouldFindOrderProductById() throws Exception {
+        OrderProduct orderProduct = orderProductList.get(0);
+        Long orderProductId = orderProduct.getId();
+
+        this.mockMvc
+                .perform(get("/OrderProduct/{id}", orderProductId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(orderProduct.getId()), Long.class))
+                .andExpect(jsonPath("$.text", is(orderProduct.getText())));
+    }
+
+    @Test
+    void shouldCreateNewOrderProduct() throws Exception {
+        OrderProduct orderProduct = new OrderProduct(null, "New OrderProduct");
+        this.mockMvc
+                .perform(
+                        post("/OrderProduct")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(orderProduct)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id", notNullValue()))
+                .andExpect(jsonPath("$.text", is(orderProduct.getText())));
+    }
+
+    @Test
+    void shouldReturn400WhenCreateNewOrderProductWithoutText() throws Exception {
+        OrderProduct orderProduct = new OrderProduct(null, null);
+
+        this.mockMvc
+                .perform(
+                        post("/OrderProduct")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(orderProduct)))
+                .andExpect(status().isBadRequest())
+                .andExpect(header().string("Content-Type", is("application/problem+json")))
+                .andExpect(
+                        jsonPath(
+                                "$.type",
+                                is("https://zalando.github.io/problem/constraint-violation")))
+                .andExpect(jsonPath("$.title", is("Constraint Violation")))
+                .andExpect(jsonPath("$.status", is(400)))
+                .andExpect(jsonPath("$.violations", hasSize(1)))
+                .andExpect(jsonPath("$.violations[0].field", is("text")))
+                .andExpect(jsonPath("$.violations[0].message", is("Text cannot be empty")))
+                .andReturn();
+    }
+
+    @Test
+    void shouldUpdateOrderProduct() throws Exception {
+        OrderProduct orderProduct = orderProductList.get(0);
+        orderProduct.setText("Updated OrderProduct");
+
+        this.mockMvc
+                .perform(
+                        put("/OrderProduct/{id}", orderProduct.getId())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(orderProduct)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(orderProduct.getId()), Long.class))
+                .andExpect(jsonPath("$.text", is(orderProduct.getText())));
+    }
+
+    @Test
+    void shouldDeleteOrderProduct() throws Exception {
+        OrderProduct orderProduct = orderProductList.get(0);
+
+        this.mockMvc
+                .perform(delete("/OrderProduct/{id}", orderProduct.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(orderProduct.getId()), Long.class))
+                .andExpect(jsonPath("$.text", is(orderProduct.getText())));
+    }
+}
---
+++ src/test/java/com/unconv/spring/web/controllers/OrderProductControllerTest.java
@@ -0,0 +1,202 @@
+package com.unconv.spring.web.controllers;
+
+import static com.unconv.spring.utils.AppConstants.PROFILE_TEST;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.unconv.spring.domain.OrderProduct;
+import com.unconv.spring.model.response.PagedResult;
+import com.unconv.spring.service.OrderProductService;
+import com.unconv.spring.web.rest.OrderProductController;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.zalando.problem.jackson.ProblemModule;
+import org.zalando.problem.violations.ConstraintViolationProblemModule;
+
+@WebMvcTest(controllers = OrderProductController.class)
+@ActiveProfiles(PROFILE_TEST)
+class OrderProductControllerTest {
+
+    @Autowired private MockMvc mockMvc;
+
+    @MockBean private OrderProductService orderProductService;
+
+    @Autowired private ObjectMapper objectMapper;
+
+    private List<OrderProduct> orderProductList;
+
+    @BeforeEach
+    void setUp() {
+        this.orderProductList = new ArrayList<>();
+        this.orderProductList.add(new OrderProduct(1L, "text 1"));
+        this.orderProductList.add(new OrderProduct(2L, "text 2"));
+        this.orderProductList.add(new OrderProduct(3L, "text 3"));
+
+        objectMapper.registerModule(new ProblemModule());
+        objectMapper.registerModule(new ConstraintViolationProblemModule());
+    }
+
+    @Test
+    void shouldFetchAllOrderProducts() throws Exception {
+        Page<OrderProduct> page = new PageImpl<>(orderProductList);
+        PagedResult<OrderProduct> orderProductPagedResult = new PagedResult<>(page);
+        given(orderProductService.findAllOrderProducts(0, 10, "id", "asc"))
+                .willReturn(orderProductPagedResult);
+
+        this.mockMvc
+                .perform(get("/OrderProduct"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.size()", is(orderProductList.size())))
+                .andExpect(jsonPath("$.totalElements", is(3)))
+                .andExpect(jsonPath("$.pageNumber", is(1)))
+                .andExpect(jsonPath("$.totalPages", is(1)))
+                .andExpect(jsonPath("$.isFirst", is(true)))
+                .andExpect(jsonPath("$.isLast", is(true)))
+                .andExpect(jsonPath("$.hasNext", is(false)))
+                .andExpect(jsonPath("$.hasPrevious", is(false)));
+    }
+
+    @Test
+    void shouldFindOrderProductById() throws Exception {
+        Long orderProductId = 1L;
+        OrderProduct orderProduct = new OrderProduct(orderProductId, "text 1");
+        given(orderProductService.findOrderProductById(orderProductId))
+                .willReturn(Optional.of(orderProduct));
+
+        this.mockMvc
+                .perform(get("/OrderProduct/{id}", orderProductId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.text", is(orderProduct.getText())));
+    }
+
+    @Test
+    void shouldReturn404WhenFetchingNonExistingOrderProduct() throws Exception {
+        Long orderProductId = 1L;
+        given(orderProductService.findOrderProductById(orderProductId))
+                .willReturn(Optional.empty());
+
+        this.mockMvc
+                .perform(get("/OrderProduct/{id}", orderProductId))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void shouldCreateNewOrderProduct() throws Exception {
+        given(orderProductService.saveOrderProduct(any(OrderProduct.class)))
+                .willAnswer((invocation) -> invocation.getArgument(0));
+
+        OrderProduct orderProduct = new OrderProduct(1L, "some text");
+        this.mockMvc
+                .perform(
+                        post("/OrderProduct")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(orderProduct)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id", notNullValue()))
+                .andExpect(jsonPath("$.text", is(orderProduct.getText())));
+    }
+
+    @Test
+    void shouldReturn400WhenCreateNewOrderProductWithoutText() throws Exception {
+        OrderProduct orderProduct = new OrderProduct(null, null);
+
+        this.mockMvc
+                .perform(
+                        post("/OrderProduct")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(orderProduct)))
+                .andExpect(status().isBadRequest())
+                .andExpect(header().string("Content-Type", is("application/problem+json")))
+                .andExpect(
+                        jsonPath(
+                                "$.type",
+                                is("https://zalando.github.io/problem/constraint-violation")))
+                .andExpect(jsonPath("$.title", is("Constraint Violation")))
+                .andExpect(jsonPath("$.status", is(400)))
+                .andExpect(jsonPath("$.violations", hasSize(1)))
+                .andExpect(jsonPath("$.violations[0].field", is("text")))
+                .andExpect(jsonPath("$.violations[0].message", is("Text cannot be empty")))
+                .andReturn();
+    }
+
+    @Test
+    void shouldUpdateOrderProduct() throws Exception {
+        Long orderProductId = 1L;
+        OrderProduct orderProduct = new OrderProduct(orderProductId, "Updated text");
+        given(orderProductService.findOrderProductById(orderProductId))
+                .willReturn(Optional.of(orderProduct));
+        given(orderProductService.saveOrderProduct(any(OrderProduct.class)))
+                .willAnswer((invocation) -> invocation.getArgument(0));
+
+        this.mockMvc
+                .perform(
+                        put("/OrderProduct/{id}", orderProduct.getId())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(orderProduct)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.text", is(orderProduct.getText())));
+    }
+
+    @Test
+    void shouldReturn404WhenUpdatingNonExistingOrderProduct() throws Exception {
+        Long orderProductId = 1L;
+        given(orderProductService.findOrderProductById(orderProductId))
+                .willReturn(Optional.empty());
+        OrderProduct orderProduct = new OrderProduct(orderProductId, "Updated text");
+
+        this.mockMvc
+                .perform(
+                        put("/OrderProduct/{id}", orderProductId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(orderProduct)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void shouldDeleteOrderProduct() throws Exception {
+        Long orderProductId = 1L;
+        OrderProduct orderProduct = new OrderProduct(orderProductId, "Some text");
+        given(orderProductService.findOrderProductById(orderProductId))
+                .willReturn(Optional.of(orderProduct));
+        doNothing().when(orderProductService).deleteOrderProductById(orderProduct.getId());
+
+        this.mockMvc
+                .perform(delete("/OrderProduct/{id}", orderProduct.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.text", is(orderProduct.getText())));
+    }
+
+    @Test
+    void shouldReturn404WhenDeletingNonExistingOrderProduct() throws Exception {
+        Long orderProductId = 1L;
+        given(orderProductService.findOrderProductById(orderProductId))
+                .willReturn(Optional.empty());
+
+        this.mockMvc
+                .perform(delete("/OrderProduct/{id}", orderProductId))
+                .andExpect(status().isNotFound());
+    }
+}
