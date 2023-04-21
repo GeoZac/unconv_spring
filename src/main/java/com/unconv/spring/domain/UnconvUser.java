625e47667eef85ed762720bf0fe2c2c3e1e99198
George Zacharia <george.zcharia@gmail.com>
2023-04-21 08:27:28 +0530

    treewide: Switch to PostgreSQL

--- src/main/java/com/unconv/spring/domain/UnconvUser.java
+++ src/main/java/com/unconv/spring/domain/UnconvUser.java
@@ -24,7 +24,7 @@ public class UnconvUser {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", columnDefinition = "BINARY(16)")
+    @Column(name = "id", columnDefinition = "uuid")
     private UUID id;
 
     @Column(nullable = false)
