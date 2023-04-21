0aa86618ef614f81d27b61f3fa0965269e59b55a
George Zacharia <george.zcharia@gmail.com>
2023-04-21 08:27:28 +0530

    treewide: Switch to PostgreSQL

--- src/main/java/com/unconv/spring/domain/SensorAuthToken.java
+++ src/main/java/com/unconv/spring/domain/SensorAuthToken.java
@@ -27,7 +27,7 @@ public class SensorAuthToken {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", columnDefinition = "BINARY(16)")
+    @Column(name = "id", columnDefinition = "uuid")
     private UUID id;
 
     @Column(nullable = false)
