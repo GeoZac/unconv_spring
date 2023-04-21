15941706b0f96671f51c94618b8098c66c670705
George Zacharia <george.zcharia@gmail.com>
2023-04-21 08:27:28 +0530

    src: treewide: Adapt UUID type to PostgreSQL

--- src/main/java/com/unconv/spring/domain/UnconvRole.java
+++ src/main/java/com/unconv/spring/domain/UnconvRole.java
@@ -23,7 +23,7 @@ public class UnconvRole {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", columnDefinition = "BINARY(16)")
+    @Column(name = "id", columnDefinition = "uuid")
     private UUID id;
 
     @Column(nullable = false)
