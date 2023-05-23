9671354195590b99c28344d89d2c1ed823c26b7e
George Zacharia <george.zcharia@gmail.com>
2023-05-23 19:39:52 +0530

    Revert "security: filter: AuthenticationFilter: Drop an extraneous try-catch "
    This reverts commit c6192415e626585f094784c07a11a2b636f306f1.

--- src/main/java/com/unconv/spring/security/filter/AuthenticationFilter.java
+++ src/main/java/com/unconv/spring/security/filter/AuthenticationFilter.java
@@ -33,11 +33,15 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     public Authentication attemptAuthentication(
             HttpServletRequest request, HttpServletResponse response)
             throws AuthenticationException {
-
-        UnconvUser user = new ObjectMapper().readValue(request.getInputStream(), UnconvUser.class);
-        Authentication authentication =
-                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
-        return customAuthenticationManager.authenticate(authentication);
+        try {
+            UnconvUser user =
+                    new ObjectMapper().readValue(request.getInputStream(), UnconvUser.class);
+            Authentication authentication =
+                    new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
+            return customAuthenticationManager.authenticate(authentication);
+        } catch (IOException e) {
+            throw new AuthenticationException("Authentication failed") {};
+        }
     }
 
     @Override
@@ -49,7 +53,6 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
             throws IOException {
 
         String token = jwtUtil.generateToken((String) authResult.getPrincipal());
-
         // Create a response object
         Map<String, Object> responseBody = new HashMap<>();
         responseBody.put("token", token);
