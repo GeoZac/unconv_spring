dbc30d42d85358722eac8615f193df16d25ae8d2
George Zacharia <george.zcharia@gmail.com>
2023-06-06 20:05:17 +0530

    UnconvRole: Service|Controller : Bring in custom changes missed

--- src/main/java/com/unconv/spring/service/UnconvRoleService.java
+++ src/main/java/com/unconv/spring/service/UnconvRoleService.java
@@ -1,11 +1,15 @@
 package com.unconv.spring.service;
 
 import com.unconv.spring.domain.UnconvRole;
+import com.unconv.spring.model.response.PagedResult;
 import com.unconv.spring.persistence.UnconvRoleRepository;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,15 +17,20 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class UnconvRoleService {
 
-    private final UnconvRoleRepository unconvRoleRepository;
+    @Autowired private UnconvRoleRepository unconvRoleRepository;
 
-    @Autowired
-    public UnconvRoleService(UnconvRoleRepository unconvRoleRepository) {
-        this.unconvRoleRepository = unconvRoleRepository;
-    }
+    public PagedResult<UnconvRole> findAllUnconvRoles(
+            int pageNo, int pageSize, String sortBy, String sortDir) {
+        Sort sort =
+                sortDir.equalsIgnoreCase(Sort.Direction.ASC.name())
+                        ? Sort.by(sortBy).ascending()
+                        : Sort.by(sortBy).descending();
 
-    public List<UnconvRole> findAllUnconvRoles() {
-        return unconvRoleRepository.findAll();
+        // Create Pageable instance
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+        Page<UnconvRole> unconvRolesPage = unconvRoleRepository.findAll(pageable);
+
+        return new PagedResult<>(unconvRolesPage);
     }
 
     public Optional<UnconvRole> findUnconvRoleById(UUID id) {
