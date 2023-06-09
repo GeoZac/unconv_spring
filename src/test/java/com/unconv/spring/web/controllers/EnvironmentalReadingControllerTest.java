c5d24c78ddace37962b730ce8485aa2b65548f9c
George Zacharia <george.zcharia@gmail.com>
2023-06-09 06:20:09 +0530

    Revert "EnvironmentalReadingControllerTest: Provide a MockBean to pass tests"
    This reverts commit 758b02c1841cbef8fed447bedcfd05f36743f4fc.

--- src/test/java/com/unconv/spring/web/controllers/EnvironmentalReadingControllerTest.java
+++ src/test/java/com/unconv/spring/web/controllers/EnvironmentalReadingControllerTest.java
@@ -28,7 +28,6 @@ import com.unconv.spring.dto.EnvironmentalReadingDTO;
 import com.unconv.spring.model.response.MessageResponse;
 import com.unconv.spring.model.response.PagedResult;
 import com.unconv.spring.service.EnvironmentalReadingService;
-import com.unconv.spring.service.SensorSystemService;
 import com.unconv.spring.web.rest.EnvironmentalReadingController;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
@@ -66,8 +65,6 @@ class EnvironmentalReadingControllerTest {
     @Autowired private MockMvc mockMvc;
 
     @MockBean private EnvironmentalReadingService environmentalReadingService;
-
-    @MockBean private SensorSystemService sensorSystemService;
 
     @Autowired private ObjectMapper objectMapper;
 
