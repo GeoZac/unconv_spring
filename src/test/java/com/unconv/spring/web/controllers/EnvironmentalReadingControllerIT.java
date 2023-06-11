23a46d7f18c1e14cf6ae0342f791434b3eb85a17
George Zacharia <george.zcharia@gmail.com>
2023-06-11 17:40:50 +0530

    EnvironmentalReadingControllerIT: Add yet another test for bulk upload
    For a non-CSV file

--- src/test/java/com/unconv/spring/web/controllers/EnvironmentalReadingControllerIT.java
+++ src/test/java/com/unconv/spring/web/controllers/EnvironmentalReadingControllerIT.java
@@ -462,6 +462,54 @@ class EnvironmentalReadingControllerIT extends AbstractIntegrationTest {
     }
 
     @Test
+    void shouldReturn400WhenUploadingNewEnvironmentalReadingsWithInvalidFile() throws Exception {
+
+        UnconvUser unconvUser =
+                new UnconvUser(null, "UnconvUser", "unconvuser@email.com", "password");
+        UnconvUser savedUnconvUser =
+                unconvUserService.saveUnconvUser(unconvUser, unconvUser.getPassword());
+        SensorSystem sensorSystem =
+                new SensorSystem(UUID.randomUUID(), "Sensor system", null, savedUnconvUser);
+        SensorSystem savedSensorSystem = sensorSystemRepository.save(sensorSystem);
+
+        List<EnvironmentalReadingDTO> environmentalReadingDTOsOfSpecificSensorForBulkData =
+                Instancio.ofList(EnvironmentalReadingDTO.class)
+                        .size(5)
+                        .supply(
+                                field(EnvironmentalReadingDTO::getSensorSystem),
+                                () -> savedSensorSystem)
+                        .ignore(field(EnvironmentalReadingDTO::getId))
+                        .create();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("temperature,humidity,timestamp\n");
+        for (EnvironmentalReadingDTO environmentalReadingDTO :
+                environmentalReadingDTOsOfSpecificSensorForBulkData) {
+            stringBuilder.append(environmentalReadingDTO.toCSVString()).append("\n");
+        }
+
+        String expectedResponse = "Please upload a csv file!";
+
+        // Create a MockMultipartFile with the CSV content
+        MockMultipartFile csvFile =
+                new MockMultipartFile(
+                        "file",
+                        "test.txt",
+                        "text/plain",
+                        stringBuilder.toString().getBytes(StandardCharsets.UTF_8));
+
+        this.mockMvc
+                .perform(
+                        multipart(
+                                        "/EnvironmentalReading/Bulk/SensorSystem/{sensorSystemId}",
+                                        savedSensorSystem.getId())
+                                .file(csvFile)
+                                .with(csrf()))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$", is(expectedResponse)));
+    }
+
+    @Test
     void shouldReturn400WhenCreateNewEnvironmentalReadingWithoutText() throws Exception {
         EnvironmentalReading environmentalReading =
                 new EnvironmentalReading(UUID.randomUUID(), 0L, 0L, null, null);
