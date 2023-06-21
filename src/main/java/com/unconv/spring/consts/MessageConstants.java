5b9600b0563d17c6aedc75475f4746870f5f2012
George Zacharia <george.zcharia@gmail.com>
2023-06-21 20:00:23 +0530

    consts: Bring in a class to store Message constants
    Most of it can be reused and will help keeping tests also in sync

---
+++ src/main/java/com/unconv/spring/consts/MessageConstants.java
@@ -0,0 +1,9 @@
+package com.unconv.spring.consts;
+
+public final class MessageConstants {
+
+    // EnvironmentalReading
+    public static final String ENVT_RECORD_ACCEPTED = "Record added successfully";
+    public static final String ENVT_RECORD_REJ_USER = "User validation failed on SensorSystem";
+    public static final String ENVT_RECORD_REJ_SENS = "Unknown SensorSystem on request";
+}
