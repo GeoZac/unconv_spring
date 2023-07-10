86674143aa86413f61f73e29ee0510bf4eafbeab
George Zacharia <george.zcharia@gmail.com>
2023-07-10 14:10:37 +0530

    persistence: SensorSystemRepository: Expose a new method
    Fetches all SensorLocation associated with a UnconvUser

--- src/main/java/com/unconv/spring/persistence/SensorSystemRepository.java
+++ src/main/java/com/unconv/spring/persistence/SensorSystemRepository.java
@@ -1,11 +1,15 @@
 package com.unconv.spring.persistence;
 
+import com.unconv.spring.domain.SensorLocation;
 import com.unconv.spring.domain.SensorSystem;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SensorSystemRepository extends JpaRepository<SensorSystem, UUID> {
+    List<SensorLocation> findDistinctByUnconvUserId(UUID unconvUserId);
+
     Page<SensorSystem> findAllByUnconvUserId(UUID unconvUserId, Pageable pageable);
 }
