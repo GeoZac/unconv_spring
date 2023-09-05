3088f11116574035a5ceffec0cd4e6e9be73ea34
George Zacharia <george.zcharia@gmail.com>
2023-09-05 13:54:01 +0530

    src: dto: UnconvUserDTO: Impose constraints on UnconvUserDTO

--- src/main/java/com/unconv/spring/dto/UnconvUserDTO.java
+++ src/main/java/com/unconv/spring/dto/UnconvUserDTO.java
@@ -1,7 +1,9 @@
 package com.unconv.spring.dto;
 
 import java.util.UUID;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,11 +17,14 @@ public class UnconvUserDTO {
     private UUID id;
 
     @NotEmpty(message = "Username cannot be empty")
+    @Size(min = 6, max = 25)
     private String username;
 
     @NotEmpty(message = "E-mail cannot be empty")
+    @Email()
     private String email;
 
     @NotEmpty(message = "Password cannot be empty")
+    @Size(min = 6, max = 25)
     private String password;
 }
