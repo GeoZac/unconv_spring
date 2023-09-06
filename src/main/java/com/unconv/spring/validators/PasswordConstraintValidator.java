3fd6954d93ad6dad6556a2d9c597a5d61b45bcfa
George Zacharia <george.zcharia@gmail.com>
2023-09-06 17:19:15 +0530

    src: validators: PasswordConstraintValidator: Fix a code smell

--- src/main/java/com/unconv/spring/validators/PasswordConstraintValidator.java
+++ src/main/java/com/unconv/spring/validators/PasswordConstraintValidator.java
@@ -15,36 +15,37 @@ import org.passay.RuleResult;
 import org.passay.WhitespaceRule;
 
 public class PasswordConstraintValidator implements ConstraintValidator<ValidPassword, String> {
+    PasswordValidator validator;
 
     @Override
-    public void initialize(ValidPassword arg0) {}
+    public void initialize(ValidPassword arg0) {
+        new PasswordValidator(
+                Arrays.asList(
+                        // at least 8 characters
+                        // at most 25 characters
+                        new LengthRule(6, 25),
+
+                        // at least one upper-case character
+                        new CharacterRule(EnglishCharacterData.UpperCase, 1),
+
+                        // at least one lower-case character
+                        new CharacterRule(EnglishCharacterData.LowerCase, 1),
+
+                        // at least one digit character
+                        new CharacterRule(EnglishCharacterData.Digit, 1),
+
+                        // at least one symbol (special character)
+                        new CharacterRule(EnglishCharacterData.Special, 1),
+
+                        // no whitespace
+                        new WhitespaceRule()));
+    }
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         if (value == null) {
             return false;
         }
-        PasswordValidator validator =
-                new PasswordValidator(
-                        Arrays.asList(
-                                // at least 8 characters
-                                // at most 25 characters
-                                new LengthRule(6, 25),
-
-                                // at least one upper-case character
-                                new CharacterRule(EnglishCharacterData.UpperCase, 1),
-
-                                // at least one lower-case character
-                                new CharacterRule(EnglishCharacterData.LowerCase, 1),
-
-                                // at least one digit character
-                                new CharacterRule(EnglishCharacterData.Digit, 1),
-
-                                // at least one symbol (special character)
-                                new CharacterRule(EnglishCharacterData.Special, 1),
-
-                                // no whitespace
-                                new WhitespaceRule()));
         RuleResult result = validator.validate(new PasswordData(value));
         if (result.isValid()) {
             return true;
