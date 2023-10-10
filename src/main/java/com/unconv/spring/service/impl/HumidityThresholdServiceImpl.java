a8623366cde675546a1f20967bd33fd9782137e5
George Zacharia <george.zcharia@gmail.com>
2023-10-10 20:26:38 +0530

    src: HumidityThresholdServiceImpl: Switch to ThresholdRepository

--- src/main/java/com/unconv/spring/service/impl/HumidityThresholdServiceImpl.java
+++ src/main/java/com/unconv/spring/service/impl/HumidityThresholdServiceImpl.java
@@ -1,13 +1,17 @@
 package com.unconv.spring.service.impl;
 
 import com.unconv.spring.domain.HumidityThreshold;
+import com.unconv.spring.domain.shared.Threshold;
 import com.unconv.spring.model.response.PagedResult;
-import com.unconv.spring.persistence.HumidityThresholdRepository;
+import com.unconv.spring.persistence.ThresholdRepository;
 import com.unconv.spring.service.HumidityThresholdService;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -18,7 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class HumidityThresholdServiceImpl implements HumidityThresholdService {
 
-    @Autowired private HumidityThresholdRepository humidityThresholdRepository;
+    @Autowired private ThresholdRepository humidityThresholdRepository;
 
     @Override
     public PagedResult<HumidityThreshold> findAllHumidityThresholds(
@@ -30,15 +34,36 @@ public class HumidityThresholdServiceImpl implements HumidityThresholdService {
 
         // Create Pageable instance
         Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+        Page<Threshold> thresholdPage = humidityThresholdRepository.findAll(pageable);
+        List<HumidityThreshold> humidityThresholdList = new ArrayList<>();
+
+        for (Threshold threshold : thresholdPage) {
+            if (threshold instanceof HumidityThreshold) {
+                humidityThresholdList.add((HumidityThreshold) threshold);
+            }
+        }
+
         Page<HumidityThreshold> humidityThresholdPage =
-                humidityThresholdRepository.findAll(pageable);
+                new PageImpl<>(
+                        humidityThresholdList,
+                        thresholdPage.getPageable(),
+                        thresholdPage.getTotalElements());
 
         return new PagedResult<>(humidityThresholdPage);
     }
 
     @Override
     public Optional<HumidityThreshold> findHumidityThresholdById(UUID id) {
-        return humidityThresholdRepository.findById(id);
+        Optional<Threshold> optionalThreshold = humidityThresholdRepository.findById(id);
+
+        if (optionalThreshold.isPresent()) {
+            Threshold threshold = optionalThreshold.get();
+            if (threshold instanceof HumidityThreshold) {
+                return Optional.of((HumidityThreshold) threshold);
+            }
+        }
+
+        return Optional.empty();
     }
 
     @Override
