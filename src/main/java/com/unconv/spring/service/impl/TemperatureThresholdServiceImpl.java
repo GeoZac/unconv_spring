76992d2e2d2edbb5576240570ff67f582ed33d1b
George Zacharia <george.zcharia@gmail.com>
2023-10-10 20:27:41 +0530

    src: TemperatureThresholdServiceImpl: Switch to ThresholdRepository

--- src/main/java/com/unconv/spring/service/impl/TemperatureThresholdServiceImpl.java
+++ src/main/java/com/unconv/spring/service/impl/TemperatureThresholdServiceImpl.java
@@ -1,13 +1,17 @@
 package com.unconv.spring.service.impl;
 
 import com.unconv.spring.domain.TemperatureThreshold;
+import com.unconv.spring.domain.shared.Threshold;
 import com.unconv.spring.model.response.PagedResult;
-import com.unconv.spring.persistence.TemperatureThresholdRepository;
+import com.unconv.spring.persistence.ThresholdRepository;
 import com.unconv.spring.service.TemperatureThresholdService;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -18,7 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class TemperatureThresholdServiceImpl implements TemperatureThresholdService {
 
-    @Autowired private TemperatureThresholdRepository temperatureThresholdRepository;
+    @Autowired private ThresholdRepository temperatureThresholdRepository;
 
     @Override
     public PagedResult<TemperatureThreshold> findAllTemperatureThresholds(
@@ -30,15 +34,36 @@ public class TemperatureThresholdServiceImpl implements TemperatureThresholdServ
 
         // Create Pageable instance
         Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+        Page<Threshold> thresholdPage = temperatureThresholdRepository.findAll(pageable);
+        List<TemperatureThreshold> temperatureThresholdList = new ArrayList<>();
+
+        for (Threshold threshold : thresholdPage) {
+            if (threshold instanceof TemperatureThreshold) {
+                temperatureThresholdList.add((TemperatureThreshold) threshold);
+            }
+        }
+
         Page<TemperatureThreshold> temperatureThresholdPage =
-                temperatureThresholdRepository.findAll(pageable);
+                new PageImpl<>(
+                        temperatureThresholdList,
+                        thresholdPage.getPageable(),
+                        thresholdPage.getTotalElements());
 
         return new PagedResult<>(temperatureThresholdPage);
     }
 
     @Override
     public Optional<TemperatureThreshold> findTemperatureThresholdById(UUID id) {
-        return temperatureThresholdRepository.findById(id);
+        Optional<Threshold> optionalThreshold = temperatureThresholdRepository.findById(id);
+
+        if (optionalThreshold.isPresent()) {
+            Threshold threshold = optionalThreshold.get();
+            if (threshold instanceof TemperatureThreshold) {
+                return Optional.of((TemperatureThreshold) threshold);
+            }
+        }
+
+        return Optional.empty();
     }
 
     @Override
