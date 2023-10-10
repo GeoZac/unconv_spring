c63507ec8145eb205caddb0adc7ce4aa8d7b7146
George Zacharia <george.zcharia@gmail.com>
2023-10-10 19:46:22 +0530

    src: domain: Make Temperature & Humidity threshold extend Threshold entity
    Also create ThresholdRepository to manage Threshold entity

--- src/main/java/com/unconv/spring/domain/HumidityThreshold.java
+++ src/main/java/com/unconv/spring/domain/HumidityThreshold.java
@@ -1,36 +1,20 @@
 package com.unconv.spring.domain;
 
+import com.unconv.spring.domain.shared.Threshold;
 import java.util.UUID;
-import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "humidity_thresholds")
 @Getter
 @Setter
+@DiscriminatorValue("H")
 @NoArgsConstructor
-@AllArgsConstructor
-public class HumidityThreshold {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", columnDefinition = "BINARY(16)")
-    private UUID id;
-
-    @Column(nullable = false)
-    @NotNull(message = "Min. value cannot be empty")
-    private double minValue;
-
-    @Column(nullable = false)
-    @NotNull(message = "Max. value cannot be empty")
-    private double maxValue;
+public class HumidityThreshold extends Threshold {
+    public HumidityThreshold(UUID id, double maxValue, double minValue) {
+        super(id, maxValue, minValue);
+    }
 }
