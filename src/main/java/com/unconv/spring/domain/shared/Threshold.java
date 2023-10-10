83ee933c51a4897494f7558e1bdf85d009d8f002
George Zacharia <george.zcharia@gmail.com>
2023-10-10 19:43:16 +0530

    src: domain: shared: Threshold: Bring in new Entity to hold thresholds
    In a shared table, with a dedicated column for discriminating the type

---
+++ src/main/java/com/unconv/spring/domain/shared/Threshold.java
@@ -0,0 +1,34 @@
+package com.unconv.spring.domain.shared;
+
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "thresholds")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "threshold_type", discriminatorType = DiscriminatorType.STRING)
+public class Threshold {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", columnDefinition = "BINARY(16)", nullable = false)
+    private UUID id;
+
+    @Column private double maxValue;
+
+    @Column private double minValue;
+}
