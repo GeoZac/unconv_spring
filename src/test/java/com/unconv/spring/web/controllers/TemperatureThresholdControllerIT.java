52385c3478d1871fdcbe6859ecb60944ee144d18
George Zacharia <george.zcharia@gmail.com>
2023-10-12 20:25:25 +0530

    test: TemperatureThresholdControllerIT: Test exclusivity of TemperatureThresholdRepository

--- src/test/java/com/unconv/spring/web/controllers/TemperatureThresholdControllerIT.java
+++ src/test/java/com/unconv/spring/web/controllers/TemperatureThresholdControllerIT.java
@@ -13,7 +13,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.unconv.spring.common.AbstractIntegrationTest;
+import com.unconv.spring.domain.HumidityThreshold;
 import com.unconv.spring.domain.TemperatureThreshold;
+import com.unconv.spring.persistence.HumidityThresholdRepository;
 import com.unconv.spring.persistence.TemperatureThresholdRepository;
 import java.util.List;
 import java.util.UUID;
@@ -33,6 +35,8 @@ class TemperatureThresholdControllerIT extends AbstractIntegrationTest {
 
     @Autowired private TemperatureThresholdRepository temperatureThresholdRepository;
 
+    @Autowired private HumidityThresholdRepository humidityThresholdRepository;
+
     private List<TemperatureThreshold> temperatureThresholdList = null;
 
     @BeforeEach
@@ -46,12 +50,23 @@ class TemperatureThresholdControllerIT extends AbstractIntegrationTest {
                         .build();
 
         temperatureThresholdRepository.deleteAllInBatch();
+        final int setUpListSize = 7;
         temperatureThresholdList =
                 Instancio.ofList(TemperatureThreshold.class)
-                        .size(5)
+                        .size(setUpListSize)
                         .ignore(field(TemperatureThreshold::getId))
                         .create();
         temperatureThresholdList = temperatureThresholdRepository.saveAll(temperatureThresholdList);
+
+        assert temperatureThresholdList.size() == setUpListSize;
+
+        List<HumidityThreshold> humidityThresholdList =
+                Instancio.ofList(HumidityThreshold.class)
+                        .size(5)
+                        .ignore(field(HumidityThreshold::getId))
+                        .create();
+
+        humidityThresholdRepository.saveAll(humidityThresholdList);
     }
 
     @Test
@@ -60,7 +75,7 @@ class TemperatureThresholdControllerIT extends AbstractIntegrationTest {
                 .perform(get("/TemperatureThreshold").param("sortDir", "asc"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data.size()", is(temperatureThresholdList.size())))
-                .andExpect(jsonPath("$.totalElements", is(5)))
+                .andExpect(jsonPath("$.totalElements", is(temperatureThresholdList.size())))
                 .andExpect(jsonPath("$.pageNumber", is(1)))
                 .andExpect(jsonPath("$.totalPages", is(1)))
                 .andExpect(jsonPath("$.isFirst", is(true)))
@@ -75,7 +90,7 @@ class TemperatureThresholdControllerIT extends AbstractIntegrationTest {
                 .perform(get("/TemperatureThreshold").param("sortDir", "desc"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data.size()", is(temperatureThresholdList.size())))
-                .andExpect(jsonPath("$.totalElements", is(5)))
+                .andExpect(jsonPath("$.totalElements", is(temperatureThresholdList.size())))
                 .andExpect(jsonPath("$.pageNumber", is(1)))
                 .andExpect(jsonPath("$.totalPages", is(1)))
                 .andExpect(jsonPath("$.isFirst", is(true)))
