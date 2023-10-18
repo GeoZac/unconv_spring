d0117a0040e972eb3feabd4953456778429d894e
George Zacharia <george.zcharia@gmail.com>
2023-10-18 14:59:45 +0530

    Revert "src: validators: ThresholdValidator: Raise ConstraintViolation when required"
    This reverts commit 6cdfe24b0f6b65c8054c3ec26f47c1aa8b9bb1bd.

--- src/main/java/com/unconv/spring/validators/ThresholdValidator.java
+++ src/main/java/com/unconv/spring/validators/ThresholdValidator.java
@@ -52,11 +52,6 @@ public class ThresholdValidator implements ConstraintValidator<ValidThreshold, O
             return minValue < maxValue;
         }
 
-        String message = context.getDefaultConstraintMessageTemplate();
-
-        context.buildConstraintViolationWithTemplate(message)
-                .addConstraintViolation()
-                .disableDefaultConstraintViolation();
         return false;
     }
 }
