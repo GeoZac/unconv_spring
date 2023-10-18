61ac8ab996a94655980406892e25a1926bffca7c
George Zacharia <george.zcharia@gmail.com>
2023-10-18 10:15:50 +0530

    src: validators: ThresholdValidator: Implement the logic for the required classes

--- src/main/java/com/unconv/spring/validators/ThresholdValidator.java
+++ src/main/java/com/unconv/spring/validators/ThresholdValidator.java
@@ -1,6 +1,10 @@
 package com.unconv.spring.validators;
 
 import com.unconv.spring.annotation.ValidThreshold;
+import com.unconv.spring.domain.HumidityThreshold;
+import com.unconv.spring.domain.TemperatureThreshold;
+import com.unconv.spring.dto.HumidityThresholdDTO;
+import com.unconv.spring.dto.TemperatureThresholdDTO;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
@@ -15,6 +19,37 @@ public class ThresholdValidator implements ConstraintValidator<ValidThreshold, O
     public boolean isValid(Object value, ConstraintValidatorContext context) {
         if (value == null) {
             return true;
+        }
+
+        if (value instanceof HumidityThreshold) {
+            HumidityThreshold humidityThreshold = (HumidityThreshold) value;
+
+            double minValue = humidityThreshold.getMinValue();
+            double maxValue = humidityThreshold.getMaxValue();
+
+            return minValue < maxValue;
+        } else if (value instanceof TemperatureThreshold) {
+            TemperatureThreshold temperatureThreshold = (TemperatureThreshold) value;
+
+            double minValue = temperatureThreshold.getMinValue();
+            double maxValue = temperatureThreshold.getMaxValue();
+
+            return minValue < maxValue;
+
+        } else if (value instanceof HumidityThresholdDTO) {
+            HumidityThresholdDTO humidityThresholdDTO = (HumidityThresholdDTO) value;
+
+            double minValue = humidityThresholdDTO.getMinValue();
+            double maxValue = humidityThresholdDTO.getMaxValue();
+
+            return minValue < maxValue;
+        } else if (value instanceof TemperatureThresholdDTO) {
+            TemperatureThresholdDTO temperatureThresholdDTO = (TemperatureThresholdDTO) value;
+
+            double minValue = temperatureThresholdDTO.getMinValue();
+            double maxValue = temperatureThresholdDTO.getMaxValue();
+
+            return minValue < maxValue;
         }
 
         return false;
