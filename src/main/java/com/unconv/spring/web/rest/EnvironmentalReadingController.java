03cb23255c49906bf55739eb410f7d7f8c7c3a65
George Zacharia <george.zcharia@gmail.com>
2023-10-22 22:23:49 +0530

    src: web: EnvironmentalReadingStatsController: Init
    Also remove dead code from EnvironmentalReadingController

--- src/main/java/com/unconv/spring/web/rest/EnvironmentalReadingController.java
+++ src/main/java/com/unconv/spring/web/rest/EnvironmentalReadingController.java
@@ -6,9 +6,7 @@ import com.unconv.spring.model.response.MessageResponse;
 import com.unconv.spring.model.response.PagedResult;
 import com.unconv.spring.service.EnvironmentalReadingService;
 import com.unconv.spring.utils.AppConstants;
-import java.time.OffsetDateTime;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
@@ -151,29 +149,5 @@ public class EnvironmentalReadingController {
                             return ResponseEntity.ok(environmentalReading);
                         })
                 .orElseGet(() -> ResponseEntity.notFound().build());
-    }
-
-    @GetMapping("/QuarterHourly/SensorSystem/{sensorSystemId}")
-    public ResponseEntity<Map<OffsetDateTime, Double>> getQuarterHourlyTemperature(
-            @PathVariable UUID sensorSystemId) {
-        Map<OffsetDateTime, Double> tenMinuteTemperatures =
-                environmentalReadingService.getAverageTempsForQuarterHourly(sensorSystemId);
-        return ResponseEntity.ok(tenMinuteTemperatures);
-    }
-
-    @GetMapping("/Hourly/SensorSystem/{sensorSystemId}")
-    public ResponseEntity<Map<OffsetDateTime, Double>> getHourlyTemperature(
-            @PathVariable UUID sensorSystemId) {
-        Map<OffsetDateTime, Double> hourlyTemperatures =
-                environmentalReadingService.getAverageTempsForHourly(sensorSystemId);
-        return ResponseEntity.ok(hourlyTemperatures);
-    }
-
-    @GetMapping("/Daily/SensorSystem/{sensorSystemId}")
-    public ResponseEntity<Map<OffsetDateTime, Double>> getDailyTemperature(
-            @PathVariable UUID sensorSystemId) {
-        Map<OffsetDateTime, Double> hourlyTemperatures =
-                environmentalReadingService.getAverageTempsForDaily(sensorSystemId);
-        return ResponseEntity.ok(hourlyTemperatures);
     }
 }
