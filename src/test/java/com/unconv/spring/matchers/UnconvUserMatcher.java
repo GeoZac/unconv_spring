c55cb048cd78b0c121aab99963c03bb3cac24f78
George Zacharia <george.zcharia@gmail.com>
2024-01-17 19:42:34 +0530

    test: matchers: UnconvUserMatcher: Bring in come custom validations

--- src/test/java/com/unconv/spring/matchers/UnconvUserMatcher.java
+++ src/test/java/com/unconv/spring/matchers/UnconvUserMatcher.java
@@ -3,6 +3,7 @@ package com.unconv.spring.matchers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.unconv.spring.domain.UnconvUser;
 import java.util.Map;
+import java.util.UUID;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
@@ -16,7 +17,27 @@ public class UnconvUserMatcher {
             @Override
             protected boolean matchesSafely(Object object) {
                 UnconvUser unconvUser = convertToUnconvUser((Map<?, ?>) object);
-                return unconvUser != null;
+                return unconvUser != null
+                        && isValidId(unconvUser.getId())
+                        && isValidUsername(unconvUser.getUsername())
+                        && isValidEmail(unconvUser.getEmail())
+                        && containsNoPassword(unconvUser.getPassword());
+            }
+
+            private boolean isValidId(UUID id) {
+                return id != null && id.version() == 4;
+            }
+
+            private boolean isValidUsername(String username) {
+                return username != null && !username.isEmpty() && !username.isBlank();
+            }
+
+            private boolean isValidEmail(String email) {
+                return email != null && !email.isEmpty() && !email.isBlank();
+            }
+
+            private boolean containsNoPassword(String password) {
+                return password == null;
             }
 
             @Override
