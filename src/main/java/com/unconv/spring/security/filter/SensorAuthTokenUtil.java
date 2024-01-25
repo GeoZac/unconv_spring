83f446fff44cb4f66f0e52a39abe1bda34f24cf8
George Zacharia <george.zcharia@gmail.com>
2024-01-25 20:03:05 +0530

    src: security: filter: SensorAuthTokenUtil: Wire up with custom logic
    Fetch the SensorSystem's user, also expose a method at SensorAuthTokenRepository for this purpose

--- src/main/java/com/unconv/spring/security/filter/SensorAuthTokenUtil.java
+++ src/main/java/com/unconv/spring/security/filter/SensorAuthTokenUtil.java
@@ -1,13 +1,30 @@
 package com.unconv.spring.security.filter;
 
+import com.unconv.spring.domain.SensorAuthToken;
+import com.unconv.spring.persistence.SensorAuthTokenRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class SensorAuthTokenUtil {
 
+    private final SensorAuthTokenRepository sensorAuthTokenRepository;
+
+    @Autowired
+    public SensorAuthTokenUtil(SensorAuthTokenRepository sensorAuthTokenRepository) {
+        this.sensorAuthTokenRepository = sensorAuthTokenRepository;
+    }
+
     String validateTokenAndRetrieveUser(String accessToken) {
-        return null;
+        SensorAuthToken sensorAuthToken = sensorAuthTokenRepository.findByAuthToken(accessToken);
+
+        if (sensorAuthToken == null) {
+            log.error("Request with unknown API token");
+            return null;
+        }
+
+        return sensorAuthToken.getSensorSystem().getUnconvUser().getUsername();
     }
 }
