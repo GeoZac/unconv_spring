d6fc356c7f6c2989187d45cd7b9e5809540c8d4f
George Zacharia <george.zcharia@gmail.com>
2024-01-25 19:57:58 +0530

    src: security: filter: SensorAuthTokenUtil: Bring in class to handle sensor auth via token

---
+++ src/main/java/com/unconv/spring/security/filter/SensorAuthTokenUtil.java
@@ -0,0 +1,13 @@
+package com.unconv.spring.security.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class SensorAuthTokenUtil {
+
+    String validateTokenAndRetrieveUser(String accessToken) {
+        return null;
+    }
+}
