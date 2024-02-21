e8685116bfe9e34bce0c66b1afcdf74ba0ac99c3
George Zacharia <george.zcharia@gmail.com>
2024-02-21 19:29:24 +0530

    src: security: filter: SensorAuthTokenUtil: Actually match the encoded token

--- src/main/java/com/unconv/spring/security/filter/SensorAuthTokenUtil.java
+++ src/main/java/com/unconv/spring/security/filter/SensorAuthTokenUtil.java
@@ -4,6 +4,8 @@ import com.unconv.spring.domain.SensorAuthToken;
 import com.unconv.spring.persistence.SensorAuthTokenRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -27,6 +29,17 @@ public class SensorAuthTokenUtil {
             return null;
         }
 
+        // Actually match the token
+        if (!bCryptPasswordEncoder().matches(accessToken, sensorAuthToken.getAuthToken())) {
+            log.error("Request with mismatched token");
+            return null;
+        }
+
         return sensorAuthToken.getSensorSystem().getUnconvUser().getUsername();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
