96b99366d4b49fb0aa3551d68395182f96bc3edd
George Zacharia <george.zcharia@gmail.com>
2024-03-15 19:10:26 +0530

    src: utils: AccessTokenGenerator: Expose some constants

--- src/main/java/com/unconv/spring/utils/AccessTokenGenerator.java
+++ src/main/java/com/unconv/spring/utils/AccessTokenGenerator.java
@@ -4,8 +4,8 @@ import org.apache.commons.lang3.RandomStringUtils;
 
 public class AccessTokenGenerator {
 
-    private static final String TOKEN_PREFIX = "UNCONV";
-    private static final int TOKEN_LENGTH = 25 - TOKEN_PREFIX.length();
+    public static final String TOKEN_PREFIX = "UNCONV";
+    public static final int TOKEN_LENGTH = 25 - TOKEN_PREFIX.length();
 
     public static String generateAccessToken() {
         String randomPart = RandomStringUtils.randomAlphanumeric(TOKEN_LENGTH);
