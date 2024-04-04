15f91b19127cd2e20e5f3ab1786751a251c2c597
George Zacharia <george.zcharia@gmail.com>
2024-04-04 07:08:26 +0530

    src: security: filter: SensorAuthTokenUtil: Check if token is expired

--- src/main/java/com/unconv/spring/security/filter/SensorAuthTokenUtil.java
+++ src/main/java/com/unconv/spring/security/filter/SensorAuthTokenUtil.java
@@ -1,9 +1,11 @@
 package com.unconv.spring.security.filter;
 
 import com.unconv.spring.domain.SensorAuthToken;
+import com.unconv.spring.exception.ExpiredAuthTokenException;
 import com.unconv.spring.exception.MalformedAuthTokenException;
 import com.unconv.spring.exception.UnknownAuthTokenException;
 import com.unconv.spring.persistence.SensorAuthTokenRepository;
+import java.time.OffsetDateTime;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -37,7 +39,16 @@ public class SensorAuthTokenUtil {
             throw new MalformedAuthTokenException(accessToken);
         }
 
+        if (isExpired(sensorAuthToken.getExpiry())) {
+            throw new ExpiredAuthTokenException(accessToken);
+        }
+
         return sensorAuthToken.getSensorSystem().getUnconvUser().getUsername();
+    }
+
+    private static boolean isExpired(OffsetDateTime expiryDateTime) {
+        OffsetDateTime currentDateTime = OffsetDateTime.now();
+        return currentDateTime.isAfter(expiryDateTime);
     }
 
     @Bean
