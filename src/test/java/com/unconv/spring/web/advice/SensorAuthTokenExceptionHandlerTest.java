9da82d2a6adfcbaf44df3e8e5b7747e1ea084a60
George Zacharia <george.zcharia@gmail.com>
2024-04-09 10:07:18 +0530

    test: SensorAuthTokenExceptionHandlerTest: Add test for expired token

--- src/test/java/com/unconv/spring/web/advice/SensorAuthTokenExceptionHandlerTest.java
+++ src/test/java/com/unconv/spring/web/advice/SensorAuthTokenExceptionHandlerTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.unconv.spring.exception.ExpiredAuthTokenException;
 import com.unconv.spring.exception.MalformedAuthTokenException;
 import com.unconv.spring.exception.UnknownAuthTokenException;
 import java.io.IOException;
@@ -52,6 +53,29 @@ class SensorAuthTokenExceptionHandlerTest {
         SensorAuthTokenExceptionHandler handler = new SensorAuthTokenExceptionHandler();
 
         MalformedAuthTokenException exception = new MalformedAuthTokenException("Malformed token");
+
+        handler.handleSensorAuthException(response, exception);
+
+        String actualJson = stringWriter.toString();
+
+        Map actualMap = new ObjectMapper().readValue(actualJson, Map.class);
+        assertEquals(actualMap.get("message"), exception.getMessage());
+        assertEquals(actualMap.get("token"), exception.getToken());
+    }
+
+    @Test
+    void testHandleExpiredAuthToken() throws IOException {
+
+        HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+
+        when(response.getWriter()).thenReturn(printWriter);
+
+        SensorAuthTokenExceptionHandler handler = new SensorAuthTokenExceptionHandler();
+
+        ExpiredAuthTokenException exception = new ExpiredAuthTokenException("Expired token");
 
         handler.handleSensorAuthException(response, exception);
 
