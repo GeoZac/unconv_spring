760320a03ee829eb5d81dc592ad90b85ac448063
George Zacharia <george.zcharia@gmail.com>
2024-04-19 22:58:19 +0530

    src: exception: UnknownAuthTokenException: Add javadoc comments

--- src/main/java/com/unconv/spring/exception/UnknownAuthTokenException.java
+++ src/main/java/com/unconv/spring/exception/UnknownAuthTokenException.java
@@ -2,9 +2,18 @@ package com.unconv.spring.exception;
 
 import lombok.Getter;
 
+/**
+ * Exception thrown when an unknown sensor authentication token is encountered. Extends {@link
+ * SensorAuthTokenException}.
+ */
 @Getter
 public class UnknownAuthTokenException extends SensorAuthTokenException {
 
+    /**
+     * Constructs a new UnknownAuthTokenException with the specified token.
+     *
+     * @param token The unknown authentication token causing the exception.
+     */
     public UnknownAuthTokenException(String token) {
         super("Unknown API token", token);
     }
