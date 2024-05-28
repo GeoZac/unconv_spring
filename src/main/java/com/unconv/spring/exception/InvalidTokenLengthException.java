59054256ff3400ecb3a7bfac34710328f71710fa
George Zacharia <george.zcharia@gmail.com>
2024-05-28 07:19:33 +0530

    src: exception: InvalidTokenLengthException: Add javadoc comments

--- src/main/java/com/unconv/spring/exception/InvalidTokenLengthException.java
+++ src/main/java/com/unconv/spring/exception/InvalidTokenLengthException.java
@@ -2,7 +2,17 @@ package com.unconv.spring.exception;
 
 import static com.unconv.spring.consts.MessageConstants.SENS_AUTH_SHORT;
 
+/**
+ * Exception thrown when the length of an access token is insufficient. This exception extends
+ * {@link SensorAuthTokenException}, indicating a problem with the access token.
+ */
 public class InvalidTokenLengthException extends SensorAuthTokenException {
+
+    /**
+     * Constructs a new InvalidTokenLengthException with the specified token.
+     *
+     * @param token The access token that triggered this exception.
+     */
     public InvalidTokenLengthException(String token) {
         super(SENS_AUTH_SHORT, token);
     }
