97f9f1c6a4554bbdd5955415bac2ee17150270cf
George Zacharia <george.zcharia@gmail.com>
2024-05-29 20:34:51 +0530

    src: exception: SensorAuthTokenException: Document

--- src/main/java/com/unconv/spring/exception/SensorAuthTokenException.java
+++ src/main/java/com/unconv/spring/exception/SensorAuthTokenException.java
@@ -3,11 +3,26 @@ package com.unconv.spring.exception;
 import java.time.OffsetDateTime;
 import lombok.Getter;
 
+/**
+ * Exception thrown when an error occurs related to sensor authentication tokens. This exception
+ * extends {@link RuntimeException}, indicating that it is an unchecked exception.
+ */
 @Getter
 public class SensorAuthTokenException extends RuntimeException {
+
+    /** The authentication token associated with the exception. */
     private final String token;
+
+    /** The timestamp when the exception occurred. */
     private final OffsetDateTime time;
 
+    /**
+     * Constructs a new SensorAuthTokenException with the specified detail message and token.
+     *
+     * @param message the detail message (which is saved for later retrieval by the {@link
+     *     #getMessage()} method)
+     * @param token the authentication token associated with the exception
+     */
     public SensorAuthTokenException(String message, String token) {
         super(message);
         this.token = token;
