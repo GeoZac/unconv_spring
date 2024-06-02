379a3cef20e6c49700b5e8206939541c7a15d175
George Zacharia <george.zcharia@gmail.com>
2024-06-02 10:46:49 +0530

    fixup! src: consts: Add javadocs

--- src/main/java/com/unconv/spring/consts/SensorLocationType.java
+++ src/main/java/com/unconv/spring/consts/SensorLocationType.java
@@ -1,16 +1,10 @@
 package com.unconv.spring.consts;
 
-/**
- * Enumeration representing the location type of sensor.
- */
+/** Enumeration representing the location type of sensor. */
 public enum SensorLocationType {
-    /**
-     * Indicates that the sensor is located indoors.
-     */
+    /** Indicates that the sensor is located indoors. */
     INDOOR,
 
-    /**
-     * Indicates that the sensor is located outdoors.
-     */
+    /** Indicates that the sensor is located outdoors. */
     OUTDOOR,
 }
