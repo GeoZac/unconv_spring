bbfad752a2aedc929755c96f31494c70e31dbf4f
George Zacharia <george.zcharia@gmail.com>
2024-06-02 09:07:44 +0530

    src: consts: Add javadocs

--- src/main/java/com/unconv/spring/consts/SensorLocationType.java
+++ src/main/java/com/unconv/spring/consts/SensorLocationType.java
@@ -1,6 +1,16 @@
 package com.unconv.spring.consts;
 
+/**
+ * Enumeration representing the location type of sensor.
+ */
 public enum SensorLocationType {
+    /**
+     * Indicates that the sensor is located indoors.
+     */
     INDOOR,
+
+    /**
+     * Indicates that the sensor is located outdoors.
+     */
     OUTDOOR,
 }
--- src/main/java/com/unconv/spring/consts/SensorStatus.java
+++ src/main/java/com/unconv/spring/consts/SensorStatus.java
@@ -1,6 +1,10 @@
 package com.unconv.spring.consts;
 
+/** Enumeration representing the status of a sensor. */
 public enum SensorStatus {
+    /** Indicates that the sensor is active. */
     ACTIVE,
+
+    /** Indicates that the sensor is inactive. */
     INACTIVE,
 }
