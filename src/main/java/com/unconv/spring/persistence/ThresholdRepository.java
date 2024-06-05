c76da821f625098b66d1562629d129720eba86b0
George Zacharia <george.zcharia@gmail.com>
2024-06-05 07:46:38 +0530

    src: persistence: ThresholdRepository: Mark deprecated

--- src/main/java/com/unconv/spring/persistence/ThresholdRepository.java
+++ src/main/java/com/unconv/spring/persistence/ThresholdRepository.java
@@ -4,4 +4,9 @@ import com.unconv.spring.domain.shared.Threshold;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * @deprecated This interface is deprecated and will be removed in future versions. Please use
+ *     {@link HumidityThresholdRepository} or {@link TemperatureThresholdRepository} instead.
+ */
+@Deprecated(forRemoval = true)
 public interface ThresholdRepository extends JpaRepository<Threshold, UUID> {}
