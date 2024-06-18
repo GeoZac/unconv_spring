dd6fb77fcf094f38e3ac4ad2571a65c95dd16b85
George Zacharia <george.zcharia@gmail.com>
2024-06-18 20:23:24 +0530

    src: annotation: ValidThreshold: Add javadoc

--- src/main/java/com/unconv/spring/annotation/ValidThreshold.java
+++ src/main/java/com/unconv/spring/annotation/ValidThreshold.java
@@ -8,13 +8,56 @@ import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
+/**
+ * An annotation to validate that a minimum value is less than a maximum value within a class.
+ *
+ * <p>This annotation can be used on types (classes or interfaces). It uses the {@link
+ * ThresholdValidator} class to perform the validation logic.
+ *
+ * <p>Example usage:
+ *
+ * <pre>{@code
+ * @ValidThreshold
+ * public class Range {
+ *     private int min;
+ *     private int max;
+ *
+ *     // getters and setters
+ * }
+ * }</pre>
+ *
+ * <p>The default error message is "Min. value must be less than Max. value", but this can be
+ * overridden by specifying a custom message.
+ *
+ * <p>This annotation can also be grouped and can carry metadata information through the payload.
+ *
+ * @see ThresholdValidator
+ */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = ThresholdValidator.class)
 public @interface ValidThreshold {
+
+    /**
+     * Specifies the error message that will be returned if the validation fails.
+     *
+     * @return the error message
+     */
     String message() default "Min. value must be less than Max. value";
 
+    /**
+     * Allows the specification of validation groups, to which this constraint belongs. This must
+     * default to an empty array.
+     *
+     * @return the validation groups
+     */
     Class<?>[] groups() default {};
 
+    /**
+     * Can be used by clients of the Bean Validation API to assign custom payload objects to a
+     * constraint. This attribute must default to an empty array.
+     *
+     * @return the custom payload
+     */
     Class<? extends Payload>[] payload() default {};
 }
