650071475e0d0db8fd43968e5249386ed3f09244
George Zacharia <george.zcharia@gmail.com>
2024-06-23 10:17:45 +0530

    src: persistence: UnconvRoleRepository: Add javadoc

--- src/main/java/com/unconv/spring/persistence/UnconvRoleRepository.java
+++ src/main/java/com/unconv/spring/persistence/UnconvRoleRepository.java
@@ -4,6 +4,18 @@ import com.unconv.spring.domain.UnconvRole;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for managing {@link UnconvRole} entities.
+ *
+ * @see JpaRepository
+ */
 public interface UnconvRoleRepository extends JpaRepository<UnconvRole, UUID> {
+
+    /**
+     * Finds an {@code UnconvRole} entity by its name.
+     *
+     * @param name the name of the role to search for
+     * @return the found {@code UnconvRole} entity
+     */
     UnconvRole findByName(String name);
 }
