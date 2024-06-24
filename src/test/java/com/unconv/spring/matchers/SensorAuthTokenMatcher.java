af794e19510f3421a8425e51ecbd3bcacf66e44b
George Zacharia <george.zcharia@gmail.com>
2024-06-24 21:17:08 +0530

    test: matchers: SensorAuthTokenMatcher: Init
    Should aid in matching asserting the structure of SensorAuthtoken

---
+++ src/test/java/com/unconv/spring/matchers/SensorAuthTokenMatcher.java
@@ -0,0 +1,25 @@
+package com.unconv.spring.matchers;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class SensorAuthTokenMatcher {
+
+    public static Matcher<Object> validSensorAuthToken() {
+        return new TypeSafeMatcher<>() {
+
+            @Override
+            protected boolean matchesSafely(Object object) {
+                String sensorAuthTokenString = (String) object;
+                String maskedSensorAuthTokenPattern = "UNCONV[A-Za-z0-9*]{19}.*";
+                return sensorAuthTokenString.matches(maskedSensorAuthTokenPattern);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("A valid SensorAuthToken");
+            }
+        };
+    }
+}
