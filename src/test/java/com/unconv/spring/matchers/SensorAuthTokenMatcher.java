babf8f4f1a00e90bde258cd252145ee90aa149ca
George Zacharia <george.zcharia@gmail.com>
2024-06-25 21:11:27 +0530

    test: matchers: SensorAuthTokenMatcher: Support unmasked SensorAuthToken

--- src/test/java/com/unconv/spring/matchers/SensorAuthTokenMatcher.java
+++ src/test/java/com/unconv/spring/matchers/SensorAuthTokenMatcher.java
@@ -13,13 +13,23 @@ public class SensorAuthTokenMatcher {
             protected boolean matchesSafely(Object object) {
                 String sensorAuthTokenString = (String) object;
                 String maskedSensorAuthTokenPattern = "UNCONV[A-Za-z0-9*]{19}.*";
-                return sensorAuthTokenString.matches(maskedSensorAuthTokenPattern)
+                String unmaskedSensorAuthTokenPattern = "UNCONV[A-Za-z0-9]{19}.*";
+
+                String selectedPattern =
+                        isMaskedSensorAuthToken
+                                ? maskedSensorAuthTokenPattern
+                                : unmaskedSensorAuthTokenPattern;
+
+                return sensorAuthTokenString.matches(selectedPattern)
                         && sensorAuthTokenString.length() == 49;
             }
 
             @Override
             public void describeTo(Description description) {
-                description.appendText("A valid SensorAuthToken");
+                description
+                        .appendText("A valid SensorAuthToken matching ")
+                        .appendText(
+                                isMaskedSensorAuthToken ? "masked pattern" : "unmasked pattern");
             }
         };
     }
