6b66180a66fcec9b270da38771b975f91996de40
George Zacharia <george.zcharia@gmail.com>
2024-07-05 19:32:39 +0530

    test: ApplicationStatusController*: Update for 493de18

--- src/test/java/com/unconv/spring/web/controllers/ApplicationStatusControllerIT.java
+++ src/test/java/com/unconv/spring/web/controllers/ApplicationStatusControllerIT.java
@@ -27,6 +27,6 @@ class ApplicationStatusControllerIT extends AbstractIntegrationTest {
                 .perform(get("/public/status/version"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", is("0.0.7-HOTFIX")));
+                .andExpect(jsonPath("$", is("0.0.8")));
     }
 }
