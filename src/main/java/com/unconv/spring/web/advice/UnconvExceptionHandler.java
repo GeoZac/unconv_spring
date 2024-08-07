3631eeea5c7129ef166c7203467e9bad92d13a76
George Zacharia <george.zcharia@gmail.com>
2024-08-07 20:33:01 +0530

    src: web: advice: UnconvExceptionHandler: Log the exceptions handled

--- src/main/java/com/unconv/spring/web/advice/UnconvExceptionHandler.java
+++ src/main/java/com/unconv/spring/web/advice/UnconvExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.unconv.spring.web.advice;
 
 import java.time.OffsetDateTime;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,11 +12,15 @@ import org.zalando.problem.Status;
 import org.zalando.problem.spring.web.advice.ProblemHandling;
 
 /** Global controller advice class responsible for handling unconverted exceptions. */
+@Slf4j
 @ControllerAdvice
 public class UnconvExceptionHandler implements ProblemHandling {
     @ExceptionHandler
     public ResponseEntity<Problem> handleInsufficientAuthenticationException(
             InsufficientAuthenticationException ex, NativeWebRequest request) {
+
+        String path = request.getDescription(false).substring(4);
+        log.error("{} occurred at path: {}", ex.getMessage(), path);
 
         Problem problem =
                 Problem.builder()
@@ -23,6 +28,7 @@ public class UnconvExceptionHandler implements ProblemHandling {
                         .withTitle("Insufficient Authentication")
                         .withStatus(Status.UNAUTHORIZED)
                         .withDetail("Authentication required to access this endpoint")
+                        .with("path", path)
                         .build();
         return create(ex, problem, request);
     }
