022641e358f945cd1027dde4bccaa9d028f6e3f9
George Zacharia <george.zcharia@gmail.com>
2024-08-15 15:49:47 +0530

    src: web: advice: UnconvExceptionHandler: Add more info
    Also log the occurrence

--- src/main/java/com/unconv/spring/web/advice/UnconvExceptionHandler.java
+++ src/main/java/com/unconv/spring/web/advice/UnconvExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.unconv.spring.web.advice;
 
 import java.time.OffsetDateTime;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.zalando.problem.Status;
 import org.zalando.problem.spring.web.advice.ProblemHandling;
 
 /** Global controller advice class responsible for handling unconverted exceptions. */
+@Slf4j
 @ControllerAdvice
 @Order(Integer.MIN_VALUE)
 public class UnconvExceptionHandler implements ProblemHandling {
@@ -19,12 +21,16 @@ public class UnconvExceptionHandler implements ProblemHandling {
     @ExceptionHandler(PropertyReferenceException.class)
     public ResponseEntity<Problem> handlePropertyReferenceException(
             PropertyReferenceException ex, NativeWebRequest request) {
+        String path = request.getDescription(false).substring(4);
+        log.error("{} occurred at path: {}", ex.getMessage(), path);
 
         Problem problem =
                 Problem.builder()
                         .with("timestamp", OffsetDateTime.now())
                         .withTitle("Bad Request")
                         .withStatus(Status.BAD_REQUEST)
+                        .withDetail("Invalid property reference: " + ex.getPropertyName())
+                        .with("path", path)
                         .build();
         return create(ex, problem, request);
     }
