1b77f955ed1f2516262a218f0eb101935b14cd72
George Zacharia <george.zcharia@gmail.com>
2024-08-24 21:38:03 +0530

    src: web: advice: SecurityExceptionHandler: Implement method to handle AccessDeniedException

--- src/main/java/com/unconv/spring/web/advice/SecurityExceptionHandler.java
+++ src/main/java/com/unconv/spring/web/advice/SecurityExceptionHandler.java
@@ -1,7 +1,27 @@
 package com.unconv.spring.web.advice;
 
+import java.time.OffsetDateTime;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.zalando.problem.Problem;
+import org.zalando.problem.Status;
 import org.zalando.problem.spring.web.advice.security.SecurityAdviceTrait;
 
 @ControllerAdvice
-public class SecurityExceptionHandler implements SecurityAdviceTrait {}
+public class SecurityExceptionHandler implements SecurityAdviceTrait {
+    @Override
+    public ResponseEntity<Problem> handleAccessDenied(
+            AccessDeniedException e, NativeWebRequest request) {
+
+        Problem problem =
+                Problem.builder()
+                        .with("timestamp", OffsetDateTime.now())
+                        .withTitle("Forbidden")
+                        .withStatus(Status.FORBIDDEN)
+                        .withDetail("You are not authorized to access this endpoint")
+                        .build();
+        return create(e, problem, request);
+    }
+}
