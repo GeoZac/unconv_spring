99688640952e5df1cf092090e83de49341c98d15
George Zacharia <george.zcharia@gmail.com>
2024-08-25 08:27:09 +0530

    UnconvExceptionHandlerTest: Add test for PropertyReferenceException

--- src/test/java/com/unconv/spring/web/advice/UnconvExceptionHandlerTest.java
+++ src/test/java/com/unconv/spring/web/advice/UnconvExceptionHandlerTest.java
@@ -4,12 +4,18 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.when;
 
 import java.time.OffsetDateTime;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.mapping.PropertyPath;
+import org.springframework.data.mapping.PropertyReferenceException;
+import org.springframework.data.util.ClassTypeInformation;
+import org.springframework.data.util.TypeInformation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
@@ -46,6 +52,31 @@ class UnconvExceptionHandlerTest {
                 .isEqualTo(HttpStatus.UNAUTHORIZED.value());
         assertThat(problem.getDetail())
                 .isEqualTo("Authentication required to access this endpoint");
+        assertThat(problem.getParameters().get("path")).isEqualTo(path);
+        assertThat(problem.getParameters().get("timestamp")).isInstanceOf(OffsetDateTime.class);
+    }
+
+    @Test
+    void handlePropertyReferenceExceptionAndReturnsProblemResponse() {
+        String invalidProperty = "invalidPropertyName";
+        TypeInformation<?> typeInformation = ClassTypeInformation.from(String.class);
+        List<PropertyPath> propertyPaths = Collections.emptyList();
+        PropertyReferenceException exception =
+                new PropertyReferenceException(invalidProperty, typeInformation, propertyPaths);
+        String path = "/api/some-resource";
+        when(request.getDescription(false)).thenReturn("uri=" + path);
+
+        ResponseEntity<Problem> responseEntity =
+                handler.handlePropertyReferenceException(exception, request);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        Problem problem = responseEntity.getBody();
+        assertThat(problem).isNotNull();
+        assert problem != null;
+        assertThat(problem.getTitle()).isEqualTo("Bad Request");
+        assertThat(Objects.requireNonNull(problem.getStatus()).getStatusCode())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(problem.getDetail()).isEqualTo("Invalid property reference: " + invalidProperty);
         assertThat(problem.getParameters().get("path")).isEqualTo(path);
         assertThat(problem.getParameters().get("timestamp")).isInstanceOf(OffsetDateTime.class);
     }
