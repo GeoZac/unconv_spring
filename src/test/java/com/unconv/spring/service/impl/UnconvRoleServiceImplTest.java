e7d4a4bbd74a4b69ba9396007b5d9eaaa6121616
George Zacharia <george.zcharia@gmail.com>
2024-08-28 08:59:31 +0530

    test: UnconvRoleServiceImplTest: Init

---
+++ src/test/java/com/unconv/spring/service/impl/UnconvRoleServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.unconv.spring.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+public class UnconvRoleServiceImplTest {
+    @Test
+    void testDeleteUnconvRoleById() {}
+
+    @Test
+    void testFindAllUnconvRoles() {}
+
+    @Test
+    void testFindUnconvRoleById() {}
+
+    @Test
+    void testFindUnconvRoleByName() {}
+
+    @Test
+    void testSaveUnconvRole() {}
+}
