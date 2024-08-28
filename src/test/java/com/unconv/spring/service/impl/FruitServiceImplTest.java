24786b02921a5cb35d64cecae4f2b1e9e9a1dcd4
George Zacharia <george.zcharia@gmail.com>
2024-08-28 09:07:05 +0530

    test: FruitServiceImplTest: Init

---
+++ src/test/java/com/unconv/spring/service/impl/FruitServiceImplTest.java
@@ -0,0 +1,17 @@
+package com.unconv.spring.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+public class FruitServiceImplTest {
+    @Test
+    void testDeleteFruitById() {}
+
+    @Test
+    void testFindAllFruits() {}
+
+    @Test
+    void testFindFruitById() {}
+
+    @Test
+    void testSaveFruit() {}
+}
