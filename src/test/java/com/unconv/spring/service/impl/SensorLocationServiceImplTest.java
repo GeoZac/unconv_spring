f2b56980bb72010e2496e927c6f286cc79065fe4
George Zacharia <george.zcharia@gmail.com>
2024-08-28 08:58:43 +0530

    test: SensorLocationServiceImplTest: Init

---
+++ src/test/java/com/unconv/spring/service/impl/SensorLocationServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.unconv.spring.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+public class SensorLocationServiceImplTest {
+    @Test
+    void testDeleteSensorLocationById() {}
+
+    @Test
+    void testFindAllSensorLocations() {}
+
+    @Test
+    void testFindAllSensorLocationsByUnconvUserId() {}
+
+    @Test
+    void testFindSensorLocationById() {}
+
+    @Test
+    void testSaveSensorLocation() {}
+}
