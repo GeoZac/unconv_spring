24f1bfaa9eafededf4585cf572d64733e8b75ded
George Zacharia <george.zcharia@gmail.com>
2024-08-28 09:07:40 +0530

    test: RouteServiceImplTest: Init

---
+++ src/test/java/com/unconv/spring/service/impl/RouteServiceImplTest.java
@@ -0,0 +1,17 @@
+package com.unconv.spring.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+public class RouteServiceImplTest {
+    @Test
+    void testDeleteRouteById() {}
+
+    @Test
+    void testFindAllRoutes() {}
+
+    @Test
+    void testFindRouteById() {}
+
+    @Test
+    void testSaveRoute() {}
+}
