601a6542cfcfb1920879c47f6a5bed20253a4d3e
George Zacharia <george.zcharia@gmail.com>
2024-08-30 21:52:39 +0530

    test: service: impl: <Booking|FruitProduct>ServiceImplTest: Add tests

--- src/test/java/com/unconv/spring/service/impl/BookingServiceImplTest.java
+++ src/test/java/com/unconv/spring/service/impl/BookingServiceImplTest.java
@@ -1,24 +1,87 @@
 package com.unconv.spring.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.unconv.spring.domain.Booking;
+import com.unconv.spring.model.response.PagedResult;
+import com.unconv.spring.persistence.BookingRepository;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+@ExtendWith(MockitoExtension.class)
 class BookingServiceImplTest {
 
+    @Mock private BookingRepository bookingRepository;
+
+    @InjectMocks private BookingServiceImpl bookingService;
+
+    private Booking booking;
+    private Long bookingId;
+
     @BeforeEach
-    void setUp() {}
+    void setUp() {
+        bookingId = 1L;
+        booking = new Booking();
+        booking.setId(bookingId);
+    }
 
     @Test
-    void findAllBookings() {}
+    void findAllBookings() {
+        int pageNo = 0;
+        int pageSize = 10;
+        String sortBy = "id";
+        String sortDir = "ASC";
+        Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by(sortBy).ascending());
+        List<Booking> bookingList = Collections.singletonList(booking);
+        Page<Booking> bookingPage = new PageImpl<>(bookingList);
+
+        when(bookingRepository.findAll(any(Pageable.class))).thenReturn(bookingPage);
+
+        PagedResult<Booking> result =
+                bookingService.findAllBookings(pageNo, pageSize, sortBy, sortDir);
+
+        assertEquals(bookingList.size(), result.data().size());
+        assertEquals(bookingList.get(0).getId(), result.data().get(0).getId());
+    }
 
     @Test
-    void findBookingById() {}
+    void findBookingById() {
+        when(bookingRepository.findById(any(Long.class))).thenReturn(Optional.of(booking));
+
+        Optional<Booking> result = bookingService.findBookingById(bookingId);
+
+        assertEquals(booking.getId(), result.get().getId());
+    }
 
     @Test
-    void saveBooking() {}
+    void saveBooking() {
+        when(bookingRepository.save(any(Booking.class))).thenReturn(booking);
+
+        Booking result = bookingService.saveBooking(booking);
+
+        assertEquals(booking.getId(), result.getId());
+    }
 
     @Test
-    void deleteBookingById() {}
+    void deleteBookingById() {
+        bookingService.deleteBookingById(bookingId);
+
+        verify(bookingRepository, times(1)).deleteById(bookingId);
+    }
 }
--- src/test/java/com/unconv/spring/service/impl/FruitProductServiceImplTest.java
+++ src/test/java/com/unconv/spring/service/impl/FruitProductServiceImplTest.java
@@ -1,24 +1,76 @@
 package com.unconv.spring.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.unconv.spring.domain.FruitProduct;
+import com.unconv.spring.persistence.FruitProductRepository;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class FruitProductServiceImplTest {
 
+    @Mock private FruitProductRepository fruitProductRepository;
+
+    @InjectMocks private FruitProductServiceImpl fruitProductService;
+
+    private FruitProduct fruitProduct;
+    private Long fruitProductId;
+
     @BeforeEach
-    void setUp() {}
+    void setUp() {
+        fruitProductId = 1L;
+        fruitProduct = new FruitProduct();
+        fruitProduct.setId(fruitProductId);
+    }
 
     @Test
-    void findAllFruitProducts() {}
+    void findAllFruitProducts() {
+
+        List<FruitProduct> fruitProductList = Collections.singletonList(fruitProduct);
+
+        when(fruitProductRepository.findAll()).thenReturn(fruitProductList);
+
+        List<FruitProduct> result = fruitProductService.findAllFruitProducts();
+
+        assertEquals(fruitProductList.size(), result.size());
+        assertEquals(fruitProductList.get(0).getId(), result.get(0).getId());
+    }
 
     @Test
-    void findFruitProductById() {}
+    void findFruitProductById() {
+        when(fruitProductRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(fruitProduct));
+
+        Optional<FruitProduct> result = fruitProductService.findFruitProductById(fruitProductId);
+
+        assertEquals(fruitProduct.getId(), result.get().getId());
+    }
 
     @Test
-    void saveFruitProduct() {}
+    void saveFruitProduct() {
+        when(fruitProductRepository.save(any(FruitProduct.class))).thenReturn(fruitProduct);
+
+        FruitProduct result = fruitProductService.saveFruitProduct(fruitProduct);
+
+        assertEquals(fruitProduct.getId(), result.getId());
+    }
 
     @Test
-    void deleteFruitProductById() {}
+    void deleteFruitProductById() {
+        fruitProductService.deleteFruitProductById(fruitProductId);
+
+        verify(fruitProductRepository, times(1)).deleteById(fruitProductId);
+    }
 }
