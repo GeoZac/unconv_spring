7c331e9165885c9fff7cbb1113d99882c8f9e852
George Zacharia <george.zcharia@gmail.com>
2024-08-31 20:28:40 +0530

    test: service: impl: <Fruit|Heater>ServiceImplTest: Add tests

--- src/test/java/com/unconv/spring/service/impl/FruitServiceImplTest.java
+++ src/test/java/com/unconv/spring/service/impl/FruitServiceImplTest.java
@@ -1,24 +1,75 @@
 package com.unconv.spring.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.unconv.spring.domain.Fruit;
+import com.unconv.spring.persistence.FruitRepository;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class FruitServiceImplTest {
 
+    @Mock private FruitRepository fruitRepository;
+
+    @InjectMocks private FruitServiceImpl fruitService;
+
+    private Fruit fruit;
+    private Long fruitId;
+
     @BeforeEach
-    void setUp() {}
+    void setUp() {
+        fruitId = 1L;
+        fruit = new Fruit();
+        fruit.setId(fruitId);
+    }
 
     @Test
-    void findAllFruits() {}
+    void findAllFruits() {
+
+        List<Fruit> fruitList = Collections.singletonList(fruit);
+
+        when(fruitRepository.findAll()).thenReturn(fruitList);
+
+        List<Fruit> result = fruitService.findAllFruits();
+
+        assertEquals(fruitList.size(), result.size());
+        assertEquals(fruitList.get(0).getId(), result.get(0).getId());
+    }
 
     @Test
-    void findFruitById() {}
+    void findFruitById() {
+        when(fruitRepository.findById(any(Long.class))).thenReturn(Optional.of(fruit));
+
+        Optional<Fruit> result = fruitService.findFruitById(fruitId);
+
+        assertEquals(fruit.getId(), result.get().getId());
+    }
 
     @Test
-    void saveFruit() {}
+    void saveFruit() {
+        when(fruitRepository.save(any(Fruit.class))).thenReturn(fruit);
+
+        Fruit result = fruitService.saveFruit(fruit);
+
+        assertEquals(fruit.getId(), result.getId());
+    }
 
     @Test
-    void deleteFruitById() {}
+    void deleteFruitById() {
+        fruitService.deleteFruitById(fruitId);
+
+        verify(fruitRepository, times(1)).deleteById(fruitId);
+    }
 }
--- src/test/java/com/unconv/spring/service/impl/HeaterServiceImplTest.java
+++ src/test/java/com/unconv/spring/service/impl/HeaterServiceImplTest.java
@@ -1,24 +1,75 @@
 package com.unconv.spring.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.unconv.spring.domain.Heater;
+import com.unconv.spring.persistence.HeaterRepository;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class HeaterServiceImplTest {
 
+    @Mock private HeaterRepository heaterRepository;
+
+    @InjectMocks private HeaterServiceImpl heaterService;
+
+    private Heater heater;
+    private Long heaterId;
+
     @BeforeEach
-    void setUp() {}
+    void setUp() {
+        heaterId = 1L;
+        heater = new Heater();
+        heater.setId(heaterId);
+    }
 
     @Test
-    void findAllHeaters() {}
+    void findAllHeaters() {
+
+        List<Heater> heaterList = Collections.singletonList(heater);
+
+        when(heaterRepository.findAll()).thenReturn(heaterList);
+
+        List<Heater> result = heaterService.findAllHeaters();
+
+        assertEquals(heaterList.size(), result.size());
+        assertEquals(heaterList.get(0).getId(), result.get(0).getId());
+    }
 
     @Test
-    void findHeaterById() {}
+    void findHeaterById() {
+        when(heaterRepository.findById(any(Long.class))).thenReturn(Optional.of(heater));
+
+        Optional<Heater> result = heaterService.findHeaterById(heaterId);
+
+        assertEquals(heater.getId(), result.get().getId());
+    }
 
     @Test
-    void saveHeater() {}
+    void saveHeater() {
+        when(heaterRepository.save(any(Heater.class))).thenReturn(heater);
+
+        Heater result = heaterService.saveHeater(heater);
+
+        assertEquals(heater.getId(), result.getId());
+    }
 
     @Test
-    void deleteHeaterById() {}
+    void deleteHeaterById() {
+        heaterService.deleteHeaterById(heaterId);
+
+        verify(heaterRepository, times(1)).deleteById(heaterId);
+    }
 }
