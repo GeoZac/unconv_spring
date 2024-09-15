5c2ddf8ec63bd70d02b1b271f7e669567f9afcdd
George Zacharia <george.zcharia@gmail.com>
2024-09-15 10:05:48 +0530

    src: web: rest: ApplicationStatusController: Add a versioned endpoint  for app version

--- src/main/java/com/unconv/spring/web/rest/ApplicationStatusController.java
+++ src/main/java/com/unconv/spring/web/rest/ApplicationStatusController.java
@@ -1,7 +1,10 @@
 package com.unconv.spring.web.rest;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +24,22 @@ public class ApplicationStatusController {
     @GetMapping("version")
     public String getAppVersion() {
         return buildProperties.getVersion();
+    }
+
+    /**
+     * Retrieves the application version.
+     *
+     * <p>This endpoint provides the application version in a RESTful manner, returning a JSON
+     * response containing the version information.
+     *
+     * @return {@link ResponseEntity} containing a {@link Map} with the key "version" and the
+     *     application version as the value, along with an HTTP 200 OK status.
+     */
+    @GetMapping("v1/version")
+    public ResponseEntity<Map<String, String>> getVersion() {
+        Map<String, String> response = new HashMap<>();
+        response.put("version", buildProperties.getVersion());
+
+        return ResponseEntity.ok(response);
     }
 }
