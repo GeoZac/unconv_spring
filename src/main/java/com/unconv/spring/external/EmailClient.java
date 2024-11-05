8328aa68f9d2929063dd8592c7653341529079d4
George Zacharia <george.zcharia@gmail.com>
2024-11-05 21:19:00 +0530

    src: external: EmailClient: Make client no-op if properties are empty

--- src/main/java/com/unconv/spring/external/EmailClient.java
+++ src/main/java/com/unconv/spring/external/EmailClient.java
@@ -1,26 +1,42 @@
 package com.unconv.spring.external;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @Service
 public class EmailClient {
 
     private final JavaMailSender mailSender;
+    private final String fromAddress;
+    private final boolean emailEnabled;
 
     @Autowired
-    public EmailClient(JavaMailSender mailSender) {
+    public EmailClient(
+            JavaMailSender mailSender,
+            @Value("${spring.mail.username:}") String fromAddress,
+            @Value("${spring.mail.host:}") String mailHost) {
         this.mailSender = mailSender;
+        this.fromAddress = fromAddress;
+
+        // Check if email configuration is complete
+        this.emailEnabled = StringUtils.hasText(fromAddress) && StringUtils.hasText(mailHost);
     }
 
     public void sendEmail(String to, String subject, String text) {
+        if (!emailEnabled) {
+            // No-op if email configuration is incomplete
+            return;
+        }
+
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
-        message.setFrom("from_email@example.com");
+        message.setFrom(fromAddress);
 
         mailSender.send(message);
     }
