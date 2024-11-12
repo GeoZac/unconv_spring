c9ebc819f8acfe597e0a3b25b99c32cc89f5ac06
George Zacharia <george.zcharia@gmail.com>
2024-11-12 06:18:58 +0530

    src: external: EmailClient: Add javadoc

--- src/main/java/com/unconv/spring/external/EmailClient.java
+++ src/main/java/com/unconv/spring/external/EmailClient.java
@@ -7,6 +7,11 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+/**
+ * Service class responsible for sending emails. It utilizes the {@link JavaMailSender} to send
+ * simple email messages. This client is enabled only if the email configuration properties are
+ * provided.
+ */
 @Service
 public class EmailClient {
 
@@ -14,6 +19,15 @@ public class EmailClient {
     private final String fromAddress;
     private final boolean emailEnabled;
 
+    /**
+     * Constructs an {@code EmailClient} with the provided {@code JavaMailSender} and email
+     * properties.
+     *
+     * @param mailSender the {@link JavaMailSender} used to send emails
+     * @param fromAddress the email address used as the sender's address
+     * @param mailHost the host of the email server; used to verify if email configuration is
+     *     complete
+     */
     @Autowired
     public EmailClient(
             JavaMailSender mailSender,
@@ -26,6 +40,16 @@ public class EmailClient {
         this.emailEnabled = StringUtils.hasText(fromAddress) && StringUtils.hasText(mailHost);
     }
 
+    /**
+     * Sends an email with the specified recipient, subject, and text.
+     *
+     * <p>The email will only be sent if the email configuration is enabled. If the recipient's
+     * address is blank or email configuration is incomplete, this method performs no action.
+     *
+     * @param to the recipient's email address
+     * @param subject the subject of the email
+     * @param text the body text of the email
+     */
     public void sendEmail(String to, String subject, String text) {
         if (!emailEnabled) {
             // No-op if email configuration is incomplete
