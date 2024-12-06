d0f946f4627eab3ab4272c18fce656edc2ce359b
George Zacharia <george.zcharia@gmail.com>
2024-12-06 07:14:29 +0530

    test: SensorSystemServiceImplTest: Add tests

--- src/test/java/com/unconv/spring/service/impl/SensorSystemServiceImplTest.java
+++ src/test/java/com/unconv/spring/service/impl/SensorSystemServiceImplTest.java
@@ -1,14 +1,36 @@
 package com.unconv.spring.service.impl;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+import com.unconv.spring.domain.SensorSystem;
+import com.unconv.spring.persistence.SensorSystemRepository;
+import java.util.Optional;
+import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class SensorSystemServiceImplTest {
 
+    @Mock private SensorSystemRepository sensorSystemRepository;
+
+    @InjectMocks private SensorSystemServiceImpl sensorSystemService;
+
+    private SensorSystem sensorSystem;
+    private UUID sensorSystemId;
+
     @BeforeEach
-    void setUp() {}
+    void setUp() {
+        sensorSystemId = UUID.randomUUID();
+        sensorSystem = new SensorSystem();
+        sensorSystem.setId(sensorSystemId);
+    }
 
     @Test
     void findAllSensorSystems() {}
@@ -17,7 +39,15 @@ class SensorSystemServiceImplTest {
     void findAllSensorSystemsByUnconvUserId() {}
 
     @Test
-    void findSensorSystemById() {}
+    void findSensorSystemById() {
+        when(sensorSystemRepository.findById(any(UUID.class)))
+                .thenReturn(Optional.of(sensorSystem));
+
+        Optional<SensorSystem> result = sensorSystemService.findSensorSystemById(sensorSystemId);
+
+        assertTrue(result.isPresent());
+        assertEquals(sensorSystem.getId(), result.get().getId());
+    }
 
     @Test
     void findSensorSystemDTOById() {}
