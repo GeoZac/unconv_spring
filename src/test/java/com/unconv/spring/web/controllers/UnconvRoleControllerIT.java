cfeda80a502e1381899c2b0a7654a65cfd703abe
George Zacharia <george.zcharia@gmail.com>
2024-12-09 19:17:51 +0530

    test: UnconvRoleControllerIT: Ensure we have a clean slate for our tests

--- src/test/java/com/unconv/spring/web/controllers/UnconvRoleControllerIT.java
+++ src/test/java/com/unconv/spring/web/controllers/UnconvRoleControllerIT.java
@@ -51,6 +51,7 @@ class UnconvRoleControllerIT extends AbstractIntegrationTest {
                         .apply(springSecurity())
                         .build();
 
+        cleanUpUnconvRoles();
         assert unconvRoleRepository.findAll().size() == defaultUserRoleCount;
 
         unconvRoleList = new ArrayList<>();
@@ -235,6 +236,10 @@ class UnconvRoleControllerIT extends AbstractIntegrationTest {
 
     @AfterEach
     void tearDown() {
+        cleanUpUnconvRoles();
+    }
+
+    private void cleanUpUnconvRoles() {
         List<UnconvRole> unconvRoles = unconvRoleRepository.findAll();
         for (UnconvRole unconvRole : unconvRoles) {
             if (EnumSet.allOf(DefaultUserRole.class).toString().contains(unconvRole.getName()))
