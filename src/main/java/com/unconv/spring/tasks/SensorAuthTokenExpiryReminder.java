c03a6798ecdb8b0c1919ef071e5539bce88e9b9b
George Zacharia <george.zcharia@gmail.com>
2024-12-25 19:57:12 +0530

    src: tasks: SensorAuthTokenExpiryReminder: implement initial logic

--- src/main/java/com/unconv/spring/tasks/SensorAuthTokenExpiryReminder.java
+++ src/main/java/com/unconv/spring/tasks/SensorAuthTokenExpiryReminder.java
@@ -1,5 +1,11 @@
 package com.unconv.spring.tasks;
 
+import com.unconv.spring.domain.SensorAuthToken;
+import com.unconv.spring.service.SensorAuthTokenService;
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -8,8 +14,26 @@ import org.springframework.stereotype.Component;
 @EnableScheduling
 public class SensorAuthTokenExpiryReminder {
 
+    @Autowired private SensorAuthTokenService sensorAuthTokenService;
+
     @Scheduled(fixedRate = 604800000)
     public void remindSensorAuthTokenExpiry() {
-        // TODO Implement task
+
+        List<SensorAuthToken> sensorAuthTokenList =
+                sensorAuthTokenService.findAllSensorAuthTokens();
+        for (SensorAuthToken sensorAuthToken : sensorAuthTokenList) {
+            OffsetDateTime now = OffsetDateTime.now();
+
+            OffsetDateTime expiryTime = sensorAuthToken.getExpiry();
+            long monthsDifference = ChronoUnit.MONTHS.between(expiryTime, now);
+
+            if (Math.abs(monthsDifference) < 1) {
+                System.out.println(
+                        sensorAuthToken.getSensorSystem().getUnconvUser().getUsername()
+                                + " - "
+                                + sensorAuthToken.getId());
+                // TODO send mail
+            }
+        }
     }
 }
