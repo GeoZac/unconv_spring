3c641f470aa08c7f8d339d3fb8878330d861d872
George Zacharia <george.zcharia@gmail.com>
2025-01-03 07:08:46 +0530

    test: SensorSystemServiceImplTest: Inject modelmapper

--- src/test/java/com/unconv/spring/service/impl/SensorSystemServiceImplTest.java
+++ src/test/java/com/unconv/spring/service/impl/SensorSystemServiceImplTest.java
@@ -16,10 +16,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 
 @ExtendWith(MockitoExtension.class)
 class SensorSystemServiceImplTest {
+
+    @Spy private ModelMapper modelMapper;
 
     @Mock private SensorSystemRepository sensorSystemRepository;
 
