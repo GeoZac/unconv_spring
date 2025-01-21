3952eace6b9cab8556aeb09fc0c193273dae98b7
George Zacharia <george.zcharia@gmail.com>
2025-01-21 21:46:08 +0530

    test: UnconvUserServiceImplTest: Begin with the tests

--- src/test/java/com/unconv/spring/service/impl/UnconvUserServiceImplTest.java
+++ src/test/java/com/unconv/spring/service/impl/UnconvUserServiceImplTest.java
@@ -1,17 +1,59 @@
 package com.unconv.spring.service.impl;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+import com.unconv.spring.domain.UnconvUser;
+import com.unconv.spring.model.response.PagedResult;
+import com.unconv.spring.persistence.UnconvUserRepository;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
+@ExtendWith(MockitoExtension.class)
 class UnconvUserServiceImplTest {
 
+    @Mock private UnconvUserRepository unconvUserRepository;
+
+    @InjectMocks private UnconvUserServiceImpl unconvUserService;
+
+    private UnconvUser unconvUser;
+    private UUID unconvUserId;
+
     @BeforeEach
-    void setUp() {}
+    void setUp() {
+        unconvUserId = UUID.randomUUID();
+        unconvUser = new UnconvUser();
+        unconvUser.setId(unconvUserId);
+    }
 
     @Test
-    void findAllUnconvUsers() {}
+    void findAllUnconvUsers() {
+        int pageNo = 0;
+        int pageSize = 10;
+        String sortBy = "id";
+        String sortDir = "ASC";
+        List<UnconvUser> unconvUserList = Collections.singletonList(unconvUser);
+        Page<UnconvUser> unconvUserPage = new PageImpl<>(unconvUserList);
+
+        when(unconvUserRepository.findAll(any(Pageable.class))).thenReturn(unconvUserPage);
+
+        PagedResult<UnconvUser> result =
+                unconvUserService.findAllUnconvUsers(pageNo, pageSize, sortBy, sortDir);
+
+        assertEquals(unconvUserList.size(), result.data().size());
+        assertEquals(unconvUserList.get(0).getId(), result.data().get(0).getId());
+    }
 
     @Test
     void findUnconvUserById() {}
