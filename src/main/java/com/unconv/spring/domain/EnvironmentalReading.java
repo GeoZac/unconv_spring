4cd2c2854febd8eda76082c7809dabfbf8800b4e
George Zacharia <george.zcharia@gmail.com>
2025-04-04 22:55:48 +0530

    treewide: Migrate deprecated javax.persistence packages to jakarta.persistence
    Reference: https://docs.openrewrite.org/recipes/java/migrate/jakarta/javaxpersistencetojakartapersistence

--- src/main/java/com/unconv/spring/domain/EnvironmentalReading.java
+++ src/main/java/com/unconv/spring/domain/EnvironmentalReading.java
@@ -4,14 +4,14 @@ import static com.unconv.spring.consts.MessageConstants.ENVT_VALID_SENSOR_SYSTEM
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotNull;
