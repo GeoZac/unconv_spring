7e5d543e06ee39883bc812b4a98a05cf8c6003d0
George Zacharia <george.zcharia@gmail.com>
2025-04-08 13:43:27 +0530

    test: SensorSystemServiceImplTest: Add more sort order cases

--- src/test/java/com/unconv/spring/service/impl/SensorSystemServiceImplTest.java
+++ src/test/java/com/unconv/spring/service/impl/SensorSystemServiceImplTest.java
@@ -100,11 +100,32 @@ class SensorSystemServiceImplTest {
     }
 
     @Test
-    void findAllSensorSystemsByUnconvUserId() {
+    void findAllSensorSystemsByUnconvUserIdInAscendingOrder() {
         int pageNo = 0;
         int pageSize = 10;
         String sortBy = "id";
         String sortDir = "ASC";
+        List<SensorSystem> sensorSystemList = Collections.singletonList(sensorSystem);
+        Page<SensorSystem> sensorLocationPage = new PageImpl<>(sensorSystemList);
+
+        when(sensorSystemRepository.findByUnconvUserIdAndDeletedFalse(
+                        any(UUID.class), any(Pageable.class)))
+                .thenReturn(sensorLocationPage);
+
+        PagedResult<SensorSystemDTO> result =
+                sensorSystemService.findAllSensorSystemsByUnconvUserId(
+                        UUID.randomUUID(), pageNo, pageSize, sortBy, sortDir);
+
+        assertEquals(sensorSystemList.size(), result.data().size());
+        assertEquals(sensorSystemList.get(0).getId(), result.data().get(0).getId());
+    }
+
+    @Test
+    void findAllSensorSystemsByUnconvUserIdInDescendingOrder() {
+        int pageNo = 0;
+        int pageSize = 10;
+        String sortBy = "id";
+        String sortDir = "DESC";
         List<SensorSystem> sensorSystemList = Collections.singletonList(sensorSystem);
         Page<SensorSystem> sensorLocationPage = new PageImpl<>(sensorSystemList);
 
