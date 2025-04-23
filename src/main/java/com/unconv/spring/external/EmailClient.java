c22b47493eb4d87c8e2c768bd81c018be42d3d09
George Zacharia <george.zcharia@gmail.com>
2025-04-23 06:44:28 +0530

    src: external: EmailClient: Add new method for HTML messages

--- src/main/java/com/unconv/spring/external/EmailClient.java
+++ src/main/java/com/unconv/spring/external/EmailClient.java
@@ -1,9 +1,12 @@
 package com.unconv.spring.external;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -67,5 +70,23 @@ public class EmailClient {
         message.setFrom(fromAddress);
 
         mailSender.send(message);
+    }
+
+    public void sendEmailWithHTMLContent(String to, String subject, String htmlContent) {
+        if (!emailEnabled || to.isBlank()) {
+            return;
+        }
+
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(htmlContent, true);
+            helper.setFrom(fromAddress);
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 }
