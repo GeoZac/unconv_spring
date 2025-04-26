a317307b6e9b751429163612c1ecf888eb731b29
George Zacharia <george.zcharia@gmail.com>
2025-04-26 21:03:40 +0530

    Migrate deprecated javax.servlet packages to jakarta.servlet
    Reference: https://docs.openrewrite.org/recipes/java/migrate/jakarta/javaxservlettojakartaservlet

--- src/main/java/com/unconv/spring/web/advice/SensorAuthTokenExceptionHandler.java
+++ src/main/java/com/unconv/spring/web/advice/SensorAuthTokenExceptionHandler.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.time.OffsetDateTime;
 import java.util.HashMap;
 import java.util.Map;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
--- src/test/java/com/unconv/spring/web/advice/SensorAuthTokenExceptionHandlerTest.java
+++ src/test/java/com/unconv/spring/web/advice/SensorAuthTokenExceptionHandlerTest.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Map;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
