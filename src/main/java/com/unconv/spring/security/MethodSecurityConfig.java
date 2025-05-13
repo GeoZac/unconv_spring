49cadb1d5e3135c105dc9e8bb32ff61e7f4f56cd
George Zacharia <george.zcharia@gmail.com>
2025-05-13 06:40:20 +0530

    src: security: MethodSecurityConfig: Drop deprecated annotation

--- src/main/java/com/unconv/spring/security/MethodSecurityConfig.java
+++ src/main/java/com/unconv/spring/security/MethodSecurityConfig.java
@@ -1,9 +1,9 @@
 package com.unconv.spring.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {}
