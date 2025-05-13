b1a964ac7e8a71c4ed636158b784df116b809b9e
George Zacharia <george.zcharia@gmail.com>
2025-05-13 06:42:42 +0530

    src: security: MethodSecurityConfig: Drop redundant extension

--- src/main/java/com/unconv/spring/security/MethodSecurityConfig.java
+++ src/main/java/com/unconv/spring/security/MethodSecurityConfig.java
@@ -2,8 +2,7 @@ package com.unconv.spring.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {}
+public class MethodSecurityConfig {}
