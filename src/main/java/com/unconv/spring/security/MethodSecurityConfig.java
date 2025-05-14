cfbc020d1bf4a5060a75ea9b4b68bebd418019fd
George Zacharia <george.zcharia@gmail.com>
2025-05-14 17:36:34 +0530

    src: security: MethodSecurityConfig: Add javadoc

--- src/main/java/com/unconv/spring/security/MethodSecurityConfig.java
+++ src/main/java/com/unconv/spring/security/MethodSecurityConfig.java
@@ -3,6 +3,7 @@ package com.unconv.spring.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
+/** Enables method-level security annotations such as {@code @Secured} and {@code @RolesAllowed}. */
 @Configuration
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class MethodSecurityConfig {}
