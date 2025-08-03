5b151cfaf7b1cb6c65d0392078f276c91cfab8dc
George Zacharia <george.zcharia@gmail.com>
2025-08-03 07:59:04 +0530

    src: exception: ReadingsCSVProcessingException: Remove unused constructor to improve test coverage

--- src/main/java/com/unconv/spring/exception/ReadingsCSVProcessingException.java
+++ src/main/java/com/unconv/spring/exception/ReadingsCSVProcessingException.java
@@ -4,8 +4,4 @@ public class ReadingsCSVProcessingException extends RuntimeException {
     public ReadingsCSVProcessingException(String message) {
         super(message);
     }
-
-    public ReadingsCSVProcessingException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
