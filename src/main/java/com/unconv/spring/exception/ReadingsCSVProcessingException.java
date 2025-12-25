cd84169823b219694cde7969b151b12784bc681f
George Zacharia <george.zcharia@gmail.com>
2025-12-25 09:56:47 +0530

    src: exception: ReadingsCSVProcessingException: Add javadoc

--- src/main/java/com/unconv/spring/exception/ReadingsCSVProcessingException.java
+++ src/main/java/com/unconv/spring/exception/ReadingsCSVProcessingException.java
@@ -1,5 +1,11 @@
 package com.unconv.spring.exception;
 
+/**
+ * Exception thrown when an error occurs during the processing of readings from a CSV file.
+ *
+ * <p>This is a runtime exception that extends {@link RuntimeException}, allowing it to be thrown
+ * without being explicitly declared in method signatures.
+ */
 public class ReadingsCSVProcessingException extends RuntimeException {
     public ReadingsCSVProcessingException(String message) {
         super(message);
