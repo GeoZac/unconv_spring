79bce24dfb2aca2b835b1eecc72acc0a1cf0583d
George Zacharia <george.zcharia@gmail.com>
2026-08-11 17:28:16 +0530

    test: *ThresholdControllerTest: Remove now redundant assertions

--- src/test/java/com/unconv/spring/web/controllers/HumidityThresholdControllerTest.java
+++ src/test/java/com/unconv/spring/web/controllers/HumidityThresholdControllerTest.java
@@ -121,14 +121,7 @@ class HumidityThresholdControllerTest extends AbstractControllerTest {
                                 preprocessResponse(prettyPrint)))
                 .andExpect(status().isOk())
                 .andExpect(pagedResponse(dataSize, DEFAULT_PAGE_SIZE, totalPages, pageNo == 0))
-                .andExpect(jsonPath("$.data.size()", is(DEFAULT_PAGE_SIZE)))
-                .andExpect(jsonPath("$.totalElements", is(dataSize)))
-                .andExpect(jsonPath("$.pageNumber", is(0)))
-                .andExpect(jsonPath("$.totalPages", is(totalPages)))
-                .andExpect(jsonPath("$.isFirst", is(true)))
-                .andExpect(jsonPath("$.isLast", is(dataSize < DEFAULT_PAGE_SIZE)))
-                .andExpect(jsonPath("$.hasNext", is(dataSize > DEFAULT_PAGE_SIZE)))
-                .andExpect(jsonPath("$.hasPrevious", is(false)));
+                .andReturn();
     }
 
     @Test
